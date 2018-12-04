226d5f6da50875363403d3af89aee7187bf898c7
Caio Zullo <caiozullo@gmail.com>
2018-12-04 16:02:12 +0000

    Create the FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,12 @@
+//
+//  Copyright © 2018 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+	let id: UUID
+	let description: String?
+	let location: String?
+	let imageURL: URL
+}

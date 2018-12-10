76bca787412445353131fea789cb72aa1e059d8e
Caio Zullo <caiozullo@gmail.com>
2018-12-10 14:55:41 +0000

    Refactor test to remove duplication

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -22,32 +22,37 @@ protocol HTTPClient {
 	func get(from url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-	func get(from url: URL) {
-		requestedURL = url
-	}
-	
-	var requestedURL: URL?
-}
-
 class RemoteFeedLoaderTests: XCTestCase {
 	
 	func test_init_doesNotRequestDataFromURL() {
-		let url = URL(string: "https://a-url.com")!
-		let client = HTTPClientSpy()
-		_ = RemoteFeedLoader(url: url, client: client)
+		let (_, client) = makeSUT()
 		
 		XCTAssertNil(client.requestedURL)
 	}
 	
 	func test_load_requestDataFromURL() {
 		let url = URL(string: "https://a-given-url.com")!
-		let client = HTTPClientSpy()
-		let sut = RemoteFeedLoader(url: url, client: client)
+		let (sut, client) = makeSUT(url: url)
 		
 		sut.load()
 		
 		XCTAssertEqual(client.requestedURL, url)
 	}
 	
+	// MARK: - Helpers
+	
+	private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+		let client = HTTPClientSpy()
+		let sut = RemoteFeedLoader(url: url, client: client)
+		return (sut, client)
+	}
+
+	private class HTTPClientSpy: HTTPClient {
+		var requestedURL: URL?
+		
+		func get(from url: URL) {
+			requestedURL = url
+		}
+	}
+
 }

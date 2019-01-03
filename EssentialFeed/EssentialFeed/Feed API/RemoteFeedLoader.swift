722d154fd8b9b6a2db3d8c3ab42d4cc85f8a9f27
Caio Zullo <caiozullo@gmail.com>
2019-01-03 11:04:25 -0200

    Delivers empty items array on 200 HTTP response with JSON empty list

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -35,8 +35,12 @@ public final class RemoteFeedLoader {
 	public func load(completion: @escaping (Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case .success:
-				completion(.failure(.invalidData))
+			case let .success(data, _):
+				if let _ = try? JSONSerialization.jsonObject(with: data) {
+					completion(.success([]))
+				} else {
+					completion(.failure(.invalidData))
+				}
 			case .failure:
 				completion(.failure(.connectivity))
 			}
--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -62,6 +62,18 @@ class RemoteFeedLoaderTests: XCTestCase {
 		})
 	}
 	
+	func test_load_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() {
+		let (sut, client) = makeSUT()
+
+		var capturedResults = [RemoteFeedLoader.Result]()
+		sut.load { capturedResults.append($0) }
+		
+		let emptyListJSON = Data(bytes: "{\"items\": []}".utf8)
+		client.complete(withStatusCode: 200, data: emptyListJSON)
+		
+		XCTAssertEqual(capturedResults, [.success([])])
+	}
+	
 	// MARK: - Helpers
 	
 	private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {

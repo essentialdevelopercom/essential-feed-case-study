0b6bf9217f442f7644842f84d389263472dbf930
Caio Zullo <caiozullo@gmail.com>
2019-01-26 15:58:43 +0000

    Delivers data and response on successful HTTP URL Request

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -15,9 +15,11 @@ class URLSessionHTTPClient {
 	struct UnexpectedValuesRepresentation: Error {}
 	
 	func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-		session.dataTask(with: url) { _, _, error in
+		session.dataTask(with: url) { data, response, error in
 			if let error = error {
 				completion(.failure(error))
+			} else if let data = data, data.count > 0, let response = response as? HTTPURLResponse {
+				completion(.success(data, response))
 			} else {
 				completion(.failure(UnexpectedValuesRepresentation()))
 			}
@@ -73,6 +75,29 @@ class URLSessionHTTPClientTests: XCTestCase {
 		XCTAssertNotNil(resultErrorFor(data: anyData(), response: nonHTTPURLResponse(), error: anyNSError()))
 		XCTAssertNotNil(resultErrorFor(data: anyData(), response: anyHTTPURLResponse(), error: anyNSError()))
 		XCTAssertNotNil(resultErrorFor(data: anyData(), response: nonHTTPURLResponse(), error: nil))
+	}
+	
+	func test_getFromURL_succeedsOnHTTPURLResponseWithData() {
+		let data = anyData()
+		let response = anyHTTPURLResponse()
+		URLProtocolStub.stub(data: data, response: response, error: nil)
+		
+		let exp = expectation(description: "Wait for completion")
+		
+		makeSUT().get(from: anyURL()) { result in
+			switch result {
+			case let .success(receivedData, receivedResponse):
+				XCTAssertEqual(receivedData, data)
+				XCTAssertEqual(receivedResponse.url, response.url)
+				XCTAssertEqual(receivedResponse.statusCode, response.statusCode)
+			default:
+				XCTFail("Expected success, got \(result) instead")
+			}
+			
+			exp.fulfill()
+		}
+		
+		wait(for: [exp], timeout: 1.0)
 	}
 
 	// MARK: - Helpers

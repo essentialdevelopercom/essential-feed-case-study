badb86fefdd33bf45156308a5a6e016cb6b25938
Caio Zullo <caiozullo@gmail.com>
2019-01-31 16:52:44 +0000

    Fix typo

--- EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -21,7 +21,7 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
 			XCTAssertEqual(items[7], expectedItem(at: 7))
 			
 		case let .failure(error)?:
-			XCTFail("Exoected successful feed result, got \(error) instead")
+			XCTFail("Expected successful feed result, got \(error) instead")
 			
 		default:
 			XCTFail("Expected successful feed result, got no result instead")

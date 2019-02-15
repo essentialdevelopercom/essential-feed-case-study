cba4244cfee601a0fc2ffb9570c87f8f2230e0e3
Caio Zullo <caiozullo@gmail.com>
2019-02-15 15:19:22 +0000

    Add SaveResult type alias to protect code from potential breaking changes

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -8,12 +8,14 @@ public final class LocalFeedLoader {
 	private let store: FeedStore
 	private let currentDate: () -> Date
 	
+	public typealias SaveResult = Error?
+	
 	public init(store: FeedStore, currentDate: @escaping () -> Date) {
 		self.store = store
 		self.currentDate = currentDate
 	}
 	
-	public func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
+	public func save(_ items: [FeedItem], completion: @escaping (SaveResult) -> Void) {
 		store.deleteCachedFeed { [weak self] error in
 			guard let self = self else { return }
 			
@@ -25,7 +27,7 @@ public final class LocalFeedLoader {
 		}
 	}
 	
-	private func cache(_ items: [FeedItem], with completion: @escaping (Error?) -> Void) {
+	private func cache(_ items: [FeedItem], with completion: @escaping (SaveResult) -> Void) {
 		store.insert(items, timestamp: currentDate()) { [weak self] error in
 			guard self != nil else { return }
 			
--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -76,7 +76,7 @@ class CacheFeedUseCaseTests: XCTestCase {
 		let store = FeedStoreSpy()
 		var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
 		
-		var receivedResults = [Error?]()
+		var receivedResults = [LocalFeedLoader.SaveResult]()
 		sut?.save([uniqueItem()]) { receivedResults.append($0) }
 		
 		sut = nil
@@ -89,7 +89,7 @@ class CacheFeedUseCaseTests: XCTestCase {
 		let store = FeedStoreSpy()
 		var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
 		
-		var receivedResults = [Error?]()
+		var receivedResults = [LocalFeedLoader.SaveResult]()
 		sut?.save([uniqueItem()]) { receivedResults.append($0) }
 		
 		store.completeDeletionSuccessfully()

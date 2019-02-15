dfe3a57c84fe7036fe466858be1b74ee4533d3b1
Caio Zullo <caiozullo@gmail.com>
2019-02-15 10:53:38 +0000

    Save command succeeds on successful cache insertion

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -60,6 +60,10 @@ class FeedStore {
 	func completeInsertion(with error: Error, at index: Int = 0) {
 		insertionCompletions[index](error)
 	}
+	
+	func completeInsertionSuccessfully(at index: Int = 0) {
+		insertionCompletions[index](nil)
+	}
 }
 
 class CacheFeedUseCaseTests: XCTestCase {
@@ -136,6 +140,24 @@ class CacheFeedUseCaseTests: XCTestCase {
 		wait(for: [exp], timeout: 1.0)
 		
 		XCTAssertEqual(receivedError as NSError?, insertionError)
+	}
+	
+	func test_save_succeedsOnSuccessfulCacheInsertion() {
+		let items = [uniqueItem(), uniqueItem()]
+		let (sut, store) = makeSUT()
+		let exp = expectation(description: "Wait for save completion")
+		
+		var receivedError: Error?
+		sut.save(items) { error in
+			receivedError = error
+			exp.fulfill()
+		}
+		
+		store.completeDeletionSuccessfully()
+		store.completeInsertionSuccessfully()
+		wait(for: [exp], timeout: 1.0)
+		
+		XCTAssertNil(receivedError)
 	}
 	
 	// MARK: - Helpers

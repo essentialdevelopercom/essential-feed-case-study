ee35f3375f873c758b4f7ce3842edcec495cf851
Caio Zullo <caiozullo@gmail.com>
2019-03-25 14:49:35 +0000

    Validate cache command does not delete invalid cache after instance has been deallocated

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -50,7 +50,9 @@ public final class LocalFeedLoader {
 	}
 	
 	public func validateCache() {
-		store.retrieve { [unowned self] result in
+		store.retrieve { [weak self] result in
+			guard let self = self else { return }
+			
 			switch result {
 			case .failure:
 				self.store.deleteCachedFeed { _ in }
--- EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -66,6 +66,18 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
 		
 		XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
 	}
+	
+	func test_validateCache_doesNotDeleteInvalidCacheAfterSUTInstanceHasBeenDeallocated() {
+		let store = FeedStoreSpy()
+		var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
+		
+		sut?.validateCache()
+		
+		sut = nil
+		store.completeRetrieval(with: anyNSError())
+		
+		XCTAssertEqual(store.receivedMessages, [.retrieve])
+	}
 
 	// MARK: - Helpers
 	

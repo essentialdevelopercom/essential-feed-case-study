a8d180240592224ceca1ac264315262e807c2854
Caio Zullo <caiozullo@gmail.com>
2019-03-25 14:50:51 +0000

    Group cases to remove duplication

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -40,10 +40,7 @@ public final class LocalFeedLoader {
 			case let .found(feed, timestamp) where self.validate(timestamp):
 				completion(.success(feed.toModels()))
 				
-			case .found:
-				completion(.success([]))
-
-			case .empty:
+			case .found, .empty:
 				completion(.success([]))
 			}
 		}

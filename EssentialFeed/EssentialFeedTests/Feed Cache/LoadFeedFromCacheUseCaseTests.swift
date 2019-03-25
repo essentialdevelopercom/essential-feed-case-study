349aebcce6489cf8ad86d03525d0e9b4d58289fd
Caio Zullo <caiozullo@gmail.com>
2019-03-25 14:31:41 +0000

    Validate cache command does not delete less than seven days old cache

--- EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -89,7 +89,7 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
 		XCTAssertEqual(store.receivedMessages, [.retrieve])
 	}
 	
-	func test_load_doesNotDeleteCacheOnLessThanSevenDaysOldCache() {
+	func test_load_hasNoSideEffectsOnLessThanSevenDaysOldCache() {
 		let feed = uniqueImageFeed()
 		let fixedCurrentDate = Date()
 		let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
--- EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -30,6 +30,18 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
 		
 		XCTAssertEqual(store.receivedMessages, [.retrieve])
 	}
+	
+	func test_validateCache_doesNotDeleteLessThanSevenDaysOldCache() {
+		let feed = uniqueImageFeed()
+		let fixedCurrentDate = Date()
+		let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+		let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+		
+		sut.validateCache()
+		store.completeRetrieval(with: feed.local, timestamp: lessThanSevenDaysOldTimestamp)
+		
+		XCTAssertEqual(store.receivedMessages, [.retrieve])
+	}
 
 	// MARK: - Helpers
 	
@@ -45,4 +57,28 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
 		return NSError(domain: "any error", code: 0)
 	}
 
+	private func uniqueImage() -> FeedImage {
+		return FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())
+	}
+	
+	private func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
+		let models = [uniqueImage(), uniqueImage()]
+		let local = models.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+		return (models, local)
+	}
+	
+	private func anyURL() -> URL {
+		return URL(string: "http://any-url.com")!
+	}
+
+}
+
+private extension Date {
+	func adding(days: Int) -> Date {
+		return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+	}
+	
+	func adding(seconds: TimeInterval) -> Date {
+		return self + seconds
+	}
 }

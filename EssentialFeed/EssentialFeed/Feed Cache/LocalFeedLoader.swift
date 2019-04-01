7c1ca5b3ee5fa84ce02cdb3f4af2f3e17a5daa4c
Caio Zullo <caiozullo@gmail.com>
2019-04-01 13:52:10 +0100

    Move `FeedCachePolicy` to its own file

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -4,23 +4,6 @@
 
 import Foundation
 
-private final class FeedCachePolicy {
-	private init() {}
-	
-	private static let calendar = Calendar(identifier: .gregorian)
-	
-	private static var maxCacheAgeInDays: Int {
-		return 7
-	}
-	
-	static func validate(_ timestamp: Date, against date: Date) -> Bool {
-		guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
-			return false
-		}
-		return date < maxCacheAge
-	}
-}
-
 public final class LocalFeedLoader {
 	private let store: FeedStore
 	private let currentDate: () -> Date

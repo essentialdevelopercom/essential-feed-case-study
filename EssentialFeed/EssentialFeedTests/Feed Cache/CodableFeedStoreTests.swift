e1c3949e23f8865478a84671e6c2c05847cf7330
Caio Zullo <caiozullo@gmail.com>
2019-04-23 17:13:12 +0100

    Extract duplicate insert test code into a reusable helper method

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -89,12 +89,7 @@ class CodableFeedStoreTests: XCTestCase {
 		let feed = uniqueImageFeed().local
 		let timestamp = Date()
 		
-		let exp = expectation(description: "Wait for cache retrieval")
-		sut.insert(feed, timestamp: timestamp) { insertionError in
-			XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
-			exp.fulfill()
-		}
-		wait(for: [exp], timeout: 1.0)
+		insert((feed, timestamp), to: sut)
 		
 		expect(sut, toRetrieve: .found(feed: feed, timestamp: timestamp))
 	}
@@ -104,12 +99,7 @@ class CodableFeedStoreTests: XCTestCase {
 		let feed = uniqueImageFeed().local
 		let timestamp = Date()
 		
-		let exp = expectation(description: "Wait for cache insertion")
-		sut.insert(feed, timestamp: timestamp) { insertionError in
-			XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
-			exp.fulfill()
-		}
-		wait(for: [exp], timeout: 1.0)
+		insert((feed, timestamp), to: sut)
 		
 		expect(sut, toRetrieveTwice: .found(feed: feed, timestamp: timestamp))
 	}
@@ -122,6 +112,15 @@ class CodableFeedStoreTests: XCTestCase {
 		return sut
 	}
 	
+	private func insert(_ cache: (feed: [LocalFeedImage], timestamp: Date), to sut: CodableFeedStore) {
+		let exp = expectation(description: "Wait for cache insertion")
+		sut.insert(cache.feed, timestamp: cache.timestamp) { insertionError in
+			XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
+			exp.fulfill()
+		}
+		wait(for: [exp], timeout: 1.0)
+	}
+
 	private func expect(_ sut: CodableFeedStore, toRetrieveTwice expectedResult: RetrieveCachedFeedResult, file: StaticString = #file, line: UInt = #line) {
 		expect(sut, toRetrieve: expectedResult, file: file, line: line)
 		expect(sut, toRetrieve: expectedResult, file: file, line: line)

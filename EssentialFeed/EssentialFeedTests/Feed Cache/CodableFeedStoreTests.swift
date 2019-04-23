e9714900f63cd7f818a937dd61995173183af086
Caio Zullo <caiozullo@gmail.com>
2019-04-23 17:34:03 +0100

    Inserting to non-empty cache overrides previously inserted cache values

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -126,6 +126,20 @@ class CodableFeedStoreTests: XCTestCase {
 		expect(sut, toRetrieveTwice: .failure(anyNSError()))
 	}
 	
+	func test_insert_overridesPreviouslyInsertedCacheValues() {
+		let sut = makeSUT()
+		
+		let firstInsertionError = insert((uniqueImageFeed().local, Date()), to: sut)
+		XCTAssertNil(firstInsertionError, "Expected to insert cache successfully")
+		
+		let latestFeed = uniqueImageFeed().local
+		let latestTimestamp = Date()
+		let latestInsertionError = insert((latestFeed, latestTimestamp), to: sut)
+		
+		XCTAssertNil(latestInsertionError, "Expected to override cache successfully")
+		expect(sut, toRetrieve: .found(feed: latestFeed, timestamp: latestTimestamp))
+	}
+	
 	// - MARK: Helpers
 	
 	private func makeSUT(storeURL: URL? = nil, file: StaticString = #file, line: UInt = #line) -> CodableFeedStore {
@@ -134,13 +148,16 @@ class CodableFeedStoreTests: XCTestCase {
 		return sut
 	}
 	
-	private func insert(_ cache: (feed: [LocalFeedImage], timestamp: Date), to sut: CodableFeedStore) {
+	@discardableResult
+	private func insert(_ cache: (feed: [LocalFeedImage], timestamp: Date), to sut: CodableFeedStore) -> Error? {
 		let exp = expectation(description: "Wait for cache insertion")
-		sut.insert(cache.feed, timestamp: cache.timestamp) { insertionError in
-			XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
+		var insertionError: Error?
+		sut.insert(cache.feed, timestamp: cache.timestamp) { receivedInsertionError in
+			insertionError = receivedInsertionError
 			exp.fulfill()
 		}
 		wait(for: [exp], timeout: 1.0)
+		return insertionError
 	}
 
 	private func expect(_ sut: CodableFeedStore, toRetrieveTwice expectedResult: RetrieveCachedFeedResult, file: StaticString = #file, line: UInt = #line) {

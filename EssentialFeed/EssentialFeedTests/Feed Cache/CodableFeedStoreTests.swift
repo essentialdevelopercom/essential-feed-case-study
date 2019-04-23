7034395430c597b856210e4ff3fa62a03c221b71
Caio Zullo <caiozullo@gmail.com>
2019-04-23 17:38:09 +0100

    Deleting an already empty cache has no side-effects (cache stays empty and does not fail)

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -64,6 +64,10 @@ class CodableFeedStore {
 			completion(error)
 		}
 	}
+	
+	func deleteCachedFeed(completion: @escaping FeedStore.DeletionCompletion) {
+		completion(nil)
+	}
 }
 
 class CodableFeedStoreTests: XCTestCase {
@@ -156,6 +160,19 @@ class CodableFeedStoreTests: XCTestCase {
 		expect(sut, toRetrieve: .empty)
 	}
 	
+	func test_delete_hasNoSideEffectsOnEmptyCache() {
+		let sut = makeSUT()
+		let exp = expectation(description: "Wait for cache deletion")
+		
+		sut.deleteCachedFeed { deletionError in
+			XCTAssertNil(deletionError, "Expected empty cache deletion to succeed")
+			exp.fulfill()
+		}
+		wait(for: [exp], timeout: 1.0)
+		
+		expect(sut, toRetrieve: .empty)
+	}
+
 	// - MARK: Helpers
 	
 	private func makeSUT(storeURL: URL? = nil, file: StaticString = #file, line: UInt = #line) -> CodableFeedStore {

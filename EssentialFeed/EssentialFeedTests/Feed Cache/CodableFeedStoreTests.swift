51c4fdb7844ccf11b9d4297ec941ea610fc45462
Caio Zullo <caiozullo@gmail.com>
2019-04-25 19:30:30 +0100

    Create `FeedStoreSpecs` that must be implemented by any `FeedStore` test implementation to guarantee it meets spec

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -5,7 +5,7 @@
 import XCTest
 import EssentialFeed
 
-class CodableFeedStoreTests: XCTestCase {
+class CodableFeedStoreTests: XCTestCase, FailableFeedStoreSpecs {
 	
 	override func setUp() {
 		super.setUp()

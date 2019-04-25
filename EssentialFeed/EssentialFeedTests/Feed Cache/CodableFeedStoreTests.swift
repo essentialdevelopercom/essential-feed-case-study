47181dd2c9b41aa1959479ce165758341947ff40
Caio Zullo <caiozullo@gmail.com>
2019-04-25 19:46:24 +0100

    Extract `FailableRetrieveFeedStoreSpecs` checks into reusable assertions

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -49,7 +49,7 @@ class CodableFeedStoreTests: XCTestCase, FailableFeedStoreSpecs {
 		
 		try! "invalid data".write(to: storeURL, atomically: false, encoding: .utf8)
 		
-		expect(sut, toRetrieve: .failure(anyNSError()))
+		assertThatRetrieveDeliversFailureOnRetrievalError(on: sut)
 	}
 	
 	func test_retrieve_hasNoSideEffectsOnFailure() {
@@ -58,7 +58,7 @@ class CodableFeedStoreTests: XCTestCase, FailableFeedStoreSpecs {
 		
 		try! "invalid data".write(to: storeURL, atomically: false, encoding: .utf8)
 		
-		expect(sut, toRetrieveTwice: .failure(anyNSError()))
+		assertThatRetrieveHasNoSideEffectsOnFailure(on: sut)
 	}
 	
 	func test_insert_deliversNoErrorOnEmptyCache() {
---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/XCTestCase+FailableRetrieveFeedStoreSpecs.swift
@@ -0,0 +1,16 @@
+//
+//  Copyright © 2019 Essential Developer. All rights reserved.
+//
+
+import XCTest
+import EssentialFeed
+
+extension FailableRetrieveFeedStoreSpecs where Self: XCTestCase {
+	func assertThatRetrieveDeliversFailureOnRetrievalError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+		expect(sut, toRetrieve: .failure(anyNSError()), file: file, line: line)
+	}
+	
+	func assertThatRetrieveHasNoSideEffectsOnFailure(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+		expect(sut, toRetrieveTwice: .failure(anyNSError()), file: file, line: line)
+	}
+}

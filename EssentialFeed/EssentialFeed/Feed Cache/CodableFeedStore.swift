464c1d095245882ca4775d41b008589778fdc2ef
Caio Zullo <caiozullo@gmail.com>
2019-04-25 09:16:10 +0100

    Make `CodableFeedStore` queue concurrent to allow multiple `retrieve` to be processed in parallel (since it has no side-effects) and use `barriers` when performing side-effects to guarantee data consistency and avoid race conditions.

--- EssentialFeed/EssentialFeed/Feed Cache/CodableFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/CodableFeedStore.swift
@@ -32,7 +32,7 @@ public class CodableFeedStore: FeedStore {
 		}
 	}
 	
-	private let queue = DispatchQueue(label: "\(CodableFeedStore.self)Queue", qos: .userInitiated)
+	private let queue = DispatchQueue(label: "\(CodableFeedStore.self)Queue", qos: .userInitiated, attributes: .concurrent)
 	private let storeURL: URL
 	
 	public init(storeURL: URL) {
@@ -58,7 +58,7 @@ public class CodableFeedStore: FeedStore {
 	
 	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
 		let storeURL = self.storeURL
-		queue.async {
+		queue.async(flags: .barrier) {
 			do {
 				let encoder = JSONEncoder()
 				let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
@@ -73,7 +73,7 @@ public class CodableFeedStore: FeedStore {
 	
 	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
 		let storeURL = self.storeURL
-		queue.async {
+		queue.async(flags: .barrier) {
 			guard FileManager.default.fileExists(atPath: storeURL.path) else {
 				return completion(nil)
 			}

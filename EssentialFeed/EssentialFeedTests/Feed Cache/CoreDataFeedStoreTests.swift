b53d39836baf1e62e8c78da1081d2cf692fdf3c4
Caio Zullo <caiozullo@gmail.com>
2019-05-15 10:36:49 +0100

    Proved that `CoreDataFeedStore` side-effects run serially

--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -74,7 +74,9 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
 	}
 	
 	func test_storeSideEffects_runSerially() {
-
+		let sut = makeSUT()
+		
+		assertThatSideEffectsRunSerially(on: sut)
 	}
 
 	// - MARK: Helpers

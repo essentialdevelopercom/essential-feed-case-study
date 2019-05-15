64ae4379195e9c794a80ef1beb7d184df5b98ab2
Caio Zullo <caiozullo@gmail.com>
2019-05-15 10:03:20 +0100

    Make `storeURL` an explicit dependency so we can inject test-specific URLs (such as `/dev/null`) to avoid sharing state with production (and other tests!)

--- EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -8,8 +8,8 @@ public final class CoreDataFeedStore: FeedStore {
 	private let container: NSPersistentContainer
 	private let context: NSManagedObjectContext
 
-	public init(bundle: Bundle = .main) throws {
-		container = try NSPersistentContainer.load(modelName: "FeedStore", in: bundle)
+	public init(storeURL: URL, bundle: Bundle = .main) throws {
+		container = try NSPersistentContainer.load(modelName: "FeedStore", url: storeURL, in: bundle)
 		context = container.newBackgroundContext()
 	}
 
@@ -33,12 +33,15 @@ private extension NSPersistentContainer {
 		case failedToLoadPersistentStores(Swift.Error)
 	}
 
-	static func load(modelName name: String, in bundle: Bundle) throws -> NSPersistentContainer {
+	static func load(modelName name: String, url: URL, in bundle: Bundle) throws -> NSPersistentContainer {
 		guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
 			throw LoadingError.modelNotFound
 		}
 		
+		let description = NSPersistentStoreDescription(url: url)
 		let container = NSPersistentContainer(name: name, managedObjectModel: model)
+		container.persistentStoreDescriptions = [description]
+
 		var loadError: Swift.Error?
 		container.loadPersistentStores { loadError = $1 }
 		try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -63,7 +63,8 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
 	
 	private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
 		let storeBundle = Bundle(for: CoreDataFeedStore.self)
-		let sut = try! CoreDataFeedStore(bundle: storeBundle)
+		let storeURL = URL(fileURLWithPath: "/dev/null")
+		let sut = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
 		trackForMemoryLeaks(sut, file: file, line: line)
 		return sut
 	}

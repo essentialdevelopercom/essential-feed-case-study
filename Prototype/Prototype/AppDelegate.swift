ad09afd0a0dcb343d48ad800bf5fe770573ddbb0
Caio Zullo <caiozullo@gmail.com>
2019-06-12 16:11:01 +0100

    Add empty project for prototype

---
+++ Prototype/Prototype/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright © 2019 Essential Developer. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+	var window: UIWindow?
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		return true
+	}
+}

8660ef4d1d68eef58ec9fd419120ac5164474dfe
Caio Zullo <caiozullo@gmail.com>
2019-06-24 18:04:21 +0100

    Load feed on pull to refresh

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -6,7 +6,7 @@ import XCTest
 import UIKit
 import EssentialFeed
 
-final class FeedViewController: UIViewController {
+final class FeedViewController: UITableViewController {
 	private var loader: FeedLoader?
 	
 	convenience init(loader: FeedLoader) {
@@ -17,6 +17,12 @@ final class FeedViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
+		refreshControl = UIRefreshControl()
+		refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+		load()
+	}
+	
+	@objc private func load() {
 		loader?.load { _ in }
 	}
 }
@@ -37,6 +43,19 @@ final class FeedViewControllerTests: XCTestCase {
 		XCTAssertEqual(loader.loadCallCount, 1)
 	}
 	
+	func test_pullToRefresh_loadsFeed() {
+		let (sut, loader) = makeSUT()
+		sut.loadViewIfNeeded()
+
+		sut.refreshControl?.allTargets.forEach { target in
+			sut.refreshControl?.actions(forTarget: target, forControlEvent: .valueChanged)?.forEach {
+				(target as NSObject).perform(Selector($0))
+			}
+		}
+		
+		XCTAssertEqual(loader.loadCallCount, 2)
+	}
+
 	// MARK: - Helpers
 	
 	private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {

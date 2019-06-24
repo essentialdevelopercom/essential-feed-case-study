86476d9c26e8d6ebbc8e6952d99ed9b680e574b5
Caio Zullo <caiozullo@gmail.com>
2019-06-24 18:06:39 +0100

    Extract pull to refresh simulation into a reusable extension on `UIRefreshControl`

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -47,13 +47,11 @@ final class FeedViewControllerTests: XCTestCase {
 		let (sut, loader) = makeSUT()
 		sut.loadViewIfNeeded()
 
-		sut.refreshControl?.allTargets.forEach { target in
-			sut.refreshControl?.actions(forTarget: target, forControlEvent: .valueChanged)?.forEach {
-				(target as NSObject).perform(Selector($0))
-			}
-		}
-		
+		sut.refreshControl?.simulatePullToRefresh()
 		XCTAssertEqual(loader.loadCallCount, 2)
+		
+		sut.refreshControl?.simulatePullToRefresh()
+		XCTAssertEqual(loader.loadCallCount, 3)
 	}
 
 	// MARK: - Helpers
@@ -74,4 +72,14 @@ final class FeedViewControllerTests: XCTestCase {
 		}
 	}
 
+}
+
+private extension UIRefreshControl {
+	func simulatePullToRefresh() {
+		allTargets.forEach { target in
+			actions(forTarget: target, forControlEvent: .valueChanged)?.forEach {
+				(target as NSObject).perform(Selector($0))
+			}
+		}
+	}
 }

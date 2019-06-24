30e4fc0661b4a7113d881b944af87a8899cdfbe4
Caio Zullo <caiozullo@gmail.com>
2019-06-24 18:14:18 +0100

    Hide loading indicator on pull to refresh

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -81,6 +81,15 @@ final class FeedViewControllerTests: XCTestCase {
 		
 		XCTAssertEqual(sut.refreshControl?.isRefreshing, true)
 	}
+	
+	func test_pullToRefresh_hidesLoadingIndicatorOnLoaderCompletion() {
+		let (sut, loader) = makeSUT()
+		
+		sut.refreshControl?.simulatePullToRefresh()
+		loader.completeFeedLoading()
+		
+		XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
+	}
 
 	// MARK: - Helpers
 	

9d483dac5c9e7f2271a27e84d526fe662e666b7c
Caio Zullo <caiozullo@gmail.com>
2019-07-04 13:28:28 +0100

    Hide loading indicator on both load error and success

--- EssentialFeed/EssentialFeediOS/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/FeedViewController.swift
@@ -25,14 +25,11 @@ final public class FeedViewController: UITableViewController {
 	@objc private func load() {
 		refreshControl?.beginRefreshing()
 		loader?.load { [weak self] result in
-			switch result {
-			case let .success(feed):
+			if let feed = try? result.get() {
 				self?.tableModel = feed
 				self?.tableView.reloadData()
-				self?.refreshControl?.endRefreshing()
-			
-			case .failure: break
 			}
+			self?.refreshControl?.endRefreshing()
 		}
 	}
 	
--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -30,13 +30,13 @@ final class FeedViewControllerTests: XCTestCase {
 		XCTAssertTrue(sut.isShowingLoadingIndicator, "Expected loading indicator once view is loaded")
 		
 		loader.completeFeedLoading(at: 0)
-		XCTAssertFalse(sut.isShowingLoadingIndicator, "Expected no loading indicator once loading is completed")
+		XCTAssertFalse(sut.isShowingLoadingIndicator, "Expected no loading indicator once loading completes successfully")
 		
 		sut.simulateUserInitiatedFeedReload()
 		XCTAssertTrue(sut.isShowingLoadingIndicator, "Expected loading indicator once user initiates a reload")
 		
-		loader.completeFeedLoading(at: 1)
-		XCTAssertFalse(sut.isShowingLoadingIndicator, "Expected no loading indicator once user initiated loading is completed")
+		loader.completeFeedLoadingWithError(at: 1)
+		XCTAssertFalse(sut.isShowingLoadingIndicator, "Expected no loading indicator once user initiated loading completes with error")
 	}
 
 	func test_loadFeedCompletion_rendersSuccessfullyLoadedFeed() {

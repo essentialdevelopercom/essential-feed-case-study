58e98c7f90ef2fda773094317fd9d0892730c8ac
Caio Zullo <caiozullo@gmail.com>
2019-07-04 14:44:20 +0100

    Preload image URL when image view is near visible

--- EssentialFeed/EssentialFeediOS/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/FeedViewController.swift
@@ -15,7 +15,7 @@ public protocol FeedImageDataLoader {
 	func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
 }
 
-final public class FeedViewController: UITableViewController {
+final public class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching {
 	private var feedLoader: FeedLoader?
 	private var imageLoader: FeedImageDataLoader?
 	private var tableModel = [FeedImage]()
@@ -32,6 +32,7 @@ final public class FeedViewController: UITableViewController {
 		
 		refreshControl = UIRefreshControl()
 		refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+		tableView.prefetchDataSource = self
 		load()
 	}
 	
@@ -81,5 +82,12 @@ final public class FeedViewController: UITableViewController {
 	public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
 		tasks[indexPath]?.cancel()
 		tasks[indexPath] = nil
+	}
+	
+	public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+		indexPaths.forEach { indexPath in
+			let cellModel = tableModel[indexPath.row]
+			_ = imageLoader?.loadImageData(from: cellModel.url) { _ in }
+		}
 	}
 }
--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -203,6 +203,22 @@ final class FeedViewControllerTests: XCTestCase {
 		XCTAssertEqual(loader.loadedImageURLs, [image0.url, image1.url, image0.url, image1.url], "Expected fourth imageURL request after second view retry action")
 	}
 
+	func test_feedImageView_preloadsImageURLWhenNearVisible() {
+		let image0 = makeImage(url: URL(string: "http://url-0.com")!)
+		let image1 = makeImage(url: URL(string: "http://url-1.com")!)
+		let (sut, loader) = makeSUT()
+		
+		sut.loadViewIfNeeded()
+		loader.completeFeedLoading(with: [image0, image1])
+		XCTAssertEqual(loader.loadedImageURLs, [], "Expected no image URL requests until image is near visible")
+		
+		sut.simulateFeedImageViewNearVisible(at: 0)
+		XCTAssertEqual(loader.loadedImageURLs, [image0.url], "Expected first image URL request once first image is near visible")
+		
+		sut.simulateFeedImageViewNearVisible(at: 1)
+		XCTAssertEqual(loader.loadedImageURLs, [image0.url, image1.url], "Expected second image URL request once second image is near visible")
+	}
+	
 	// MARK: - Helpers
 	
 	private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
@@ -317,6 +333,12 @@ private extension FeedViewController {
 		delegate?.tableView?(tableView, didEndDisplaying: view!, forRowAt: index)
 	}
 	
+	func simulateFeedImageViewNearVisible(at row: Int) {
+		let ds = tableView.prefetchDataSource
+		let index = IndexPath(row: row, section: feedImagesSection)
+		ds?.tableView(tableView, prefetchRowsAt: [index])
+	}
+
 	var isShowingLoadingIndicator: Bool {
 		return refreshControl?.isRefreshing == true
 	}

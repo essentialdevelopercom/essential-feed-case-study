3022657ea8d1ea8b6e56483ce40c156886ac80d4
Caio Zullo <caiozullo@gmail.com>
2019-07-30 10:44:53 +0100

    Decouple `FeedImageViewModel` from `UIKit` by creating a transformation closure that converts an image `Data` value into a generic `Image` type. When composing with a UIKit user interface, we inject a closure to transform the image `Data` into `UIImage`.

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -5,9 +5,9 @@
 import UIKit
 
 final class FeedImageCellController {
-	private let viewModel: FeedImageViewModel
+	private let viewModel: FeedImageViewModel<UIImage>
 	
-	init(viewModel: FeedImageViewModel) {
+	init(viewModel: FeedImageViewModel<UIImage>) {
 		self.viewModel = viewModel
 	}
 	
--- EssentialFeed/EssentialFeediOS/Feed UI/Models/FeedImageViewModel.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Models/FeedImageViewModel.swift
@@ -3,19 +3,20 @@
 //
 
 import Foundation
-import UIKit
 import EssentialFeed
 
-final class FeedImageViewModel {
+final class FeedImageViewModel<Image> {
 	typealias Observer<T> = (T) -> Void
 	
 	private var task: FeedImageDataLoaderTask?
 	private let model: FeedImage
 	private let imageLoader: FeedImageDataLoader
+	private let imageTransformer: (Data) -> Image?
 	
-	init(model: FeedImage, imageLoader: FeedImageDataLoader) {
+	init(model: FeedImage, imageLoader: FeedImageDataLoader, imageTransformer: @escaping (Data) -> Image?) {
 		self.model = model
 		self.imageLoader = imageLoader
+		self.imageTransformer = imageTransformer
 	}
 	
 	var description: String? {
@@ -30,7 +31,7 @@ final class FeedImageViewModel {
 		return location != nil
 	}
 	
-	var onImageLoad: Observer<UIImage>?
+	var onImageLoad: Observer<Image>?
 	var onImageLoadingStateChange: Observer<Bool>?
 	var onShouldRetryImageLoadStateChange: Observer<Bool>?
 	
@@ -43,7 +44,7 @@ final class FeedImageViewModel {
 	}
 	
 	private func handle(_ result: FeedImageDataLoader.Result) {
-		if let image = (try? result.get()).flatMap(UIImage.init) {
+		if let image = (try? result.get()).flatMap(imageTransformer) {
 			onImageLoad?(image)
 		} else {
 			onShouldRetryImageLoadStateChange?(true)

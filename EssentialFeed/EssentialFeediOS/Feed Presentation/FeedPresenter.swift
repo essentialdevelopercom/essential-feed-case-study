7b67938d0c258893165bd91ef999bb04055105b6
Caio Zullo <caiozullo@gmail.com>
2019-08-07 17:34:33 +0100

    Decouple `FeedPresenter` from `FeedLoader` with an adapter in the Composition layer

--- EssentialFeed/EssentialFeediOS/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedPresenter.swift
@@ -21,24 +21,19 @@ protocol FeedView {
 }
 
 final class FeedPresenter {
-	typealias Observer<T> = (T) -> Void
-	
-	private let feedLoader: FeedLoader
-	
-	init(feedLoader: FeedLoader) {
-		self.feedLoader = feedLoader
-	}
-	
 	var feedView: FeedView?
 	var loadingView: FeedLoadingView?
 	
-	func loadFeed() {
+	func didStartLoadingFeed() {
 		loadingView?.display(FeedLoadingViewModel(isLoading: true))
-		feedLoader.load { [weak self] result in
-			if let feed = try? result.get() {
-				self?.feedView?.display(FeedViewModel(feed: feed))
-			}
-			self?.loadingView?.display(FeedLoadingViewModel(isLoading: false))
-		}
+	}
+	
+	func didFinishLoadingFeed(with feed: [FeedImage]) {
+		feedView?.display(FeedViewModel(feed: feed))
+		loadingView?.display(FeedLoadingViewModel(isLoading: false))
+	}
+	
+	func didFinishLoadingFeed(with error: Error) {
+		loadingView?.display(FeedLoadingViewModel(isLoading: false))
 	}
 }
--- EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -9,8 +9,9 @@ public final class FeedUIComposer {
 	private init() {}
 	
 	public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
-		let presenter = FeedPresenter(feedLoader: feedLoader)
-		let refreshController = FeedRefreshViewController(loadFeed: presenter.loadFeed)
+		let presenter = FeedPresenter()
+		let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader, presenter: presenter)
+		let refreshController = FeedRefreshViewController(loadFeed: presentationAdapter.loadFeed)
 		let feedController = FeedViewController(refreshController: refreshController)
 		presenter.loadingView = WeakRefVirtualProxy(refreshController)
 		presenter.feedView = FeedViewAdapter(controller: feedController, imageLoader: imageLoader)
@@ -54,6 +55,30 @@ private final class FeedViewAdapter: FeedView {
 		controller?.tableModel = viewModel.feed.map { model in
 			FeedImageCellController(viewModel:
 				FeedImageViewModel(model: model, imageLoader: imageLoader, imageTransformer: UIImage.init))
+		}
+	}
+}
+
+private final class FeedLoaderPresentationAdapter {
+	private let feedLoader: FeedLoader
+	private let presenter: FeedPresenter
+	
+	init(feedLoader: FeedLoader, presenter: FeedPresenter) {
+		self.feedLoader = feedLoader
+		self.presenter = presenter
+	}
+	
+	func loadFeed() {
+		presenter.didStartLoadingFeed()
+		
+		feedLoader.load { [weak self] result in
+			switch result {
+			case let .success(feed):
+				self?.presenter.didFinishLoadingFeed(with: feed)
+				
+			case let .failure(error):
+				self?.presenter.didFinishLoadingFeed(with: error)
+			}
 		}
 	}
 }

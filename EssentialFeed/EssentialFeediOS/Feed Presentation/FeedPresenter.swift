cd763d7c65c00319d4f1dcd98aa1058c03dc0cb2
Caio Zullo <caiozullo@gmail.com>
2019-08-07 17:54:58 +0100

    Move ViewModels to separate files

--- EssentialFeed/EssentialFeediOS/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedPresenter.swift
@@ -4,16 +4,8 @@
 
 import EssentialFeed
 
-struct FeedLoadingViewModel {
-	let isLoading: Bool
-}
-
 protocol FeedLoadingView {
 	func display(_ viewModel: FeedLoadingViewModel)
-}
-
-struct FeedViewModel {
-	let feed: [FeedImage]
 }
 
 protocol FeedView {
---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  Copyright © 2019 Essential Developer. All rights reserved.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+	let feed: [FeedImage]
+}

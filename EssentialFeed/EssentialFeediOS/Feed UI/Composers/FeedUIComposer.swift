e574e1e3bec39b4d84b916bca31f97b03abf1ea1
Caio Zullo <caiozullo@gmail.com>
2019-08-07 17:51:40 +0100

    Add `FeedImagePresenter` holding `FeedImage` presentation logic (including image loading state)

--- EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -19,15 +19,6 @@ public final class FeedUIComposer {
 		
 		return feedController
 	}
-	
-	private static func adaptFeedToCellControllers(forwardingTo controller: FeedViewController, loader: FeedImageDataLoader) -> ([FeedImage]) -> Void {
-		return { [weak controller] feed in
-			controller?.tableModel = feed.map { model in
-				FeedImageCellController(viewModel:
-					FeedImageViewModel(model: model, imageLoader: loader, imageTransformer: UIImage.init))
-			}
-		}
-	}
 }
 
 private final class WeakRefVirtualProxy<T: AnyObject> {
@@ -44,6 +35,12 @@ extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
 	}
 }
 
+extension WeakRefVirtualProxy: FeedImageView where T: FeedImageView, T.Image == UIImage {
+	func display(_ model: FeedImageViewModel<UIImage>) {
+		object?.display(model)
+	}
+}
+
 private final class FeedViewAdapter: FeedView {
 	private weak var controller: FeedViewController?
 	private let imageLoader: FeedImageDataLoader
@@ -52,11 +49,17 @@ private final class FeedViewAdapter: FeedView {
 		self.controller = controller
 		self.imageLoader = imageLoader
 	}
-
+	
 	func display(_ viewModel: FeedViewModel) {
 		controller?.tableModel = viewModel.feed.map { model in
-			FeedImageCellController(viewModel:
-				FeedImageViewModel(model: model, imageLoader: imageLoader, imageTransformer: UIImage.init))
+			let adapter = FeedImageDataLoaderPresentationAdapter<WeakRefVirtualProxy<FeedImageCellController>, UIImage>(model: model, imageLoader: imageLoader)
+			let view = FeedImageCellController(delegate: adapter)
+			
+			adapter.presenter = FeedImagePresenter(
+				view: WeakRefVirtualProxy(view),
+				imageTransformer: UIImage.init)
+			
+			return view
 		}
 	}
 }
@@ -81,5 +84,37 @@ private final class FeedLoaderPresentationAdapter: FeedRefreshViewControllerDele
 				self?.presenter?.didFinishLoadingFeed(with: error)
 			}
 		}
+	}
+}
+
+private final class FeedImageDataLoaderPresentationAdapter<View: FeedImageView, Image>: FeedImageCellControllerDelegate where View.Image == Image {
+	private let model: FeedImage
+	private let imageLoader: FeedImageDataLoader
+	private var task: FeedImageDataLoaderTask?
+	
+	var presenter: FeedImagePresenter<View, Image>?
+	
+	init(model: FeedImage, imageLoader: FeedImageDataLoader) {
+		self.model = model
+		self.imageLoader = imageLoader
+	}
+	
+	func didRequestImage() {
+		presenter?.didStartLoadingImageData(for: model)
+		
+		let model = self.model
+		task = imageLoader.loadImageData(from: model.url) { [weak self] result in
+			switch result {
+			case let .success(data):
+				self?.presenter?.didFinishLoadingImageData(with: data, for: model)
+				
+			case let .failure(error):
+				self?.presenter?.didFinishLoadingImageData(with: error, for: model)
+			}
+		}
+	}
+	
+	func didCancelImageRequest() {
+		task?.cancel()
 	}
 }
--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -4,45 +4,39 @@
 
 import UIKit
 
-final class FeedImageCellController {
-	private let viewModel: FeedImageViewModel<UIImage>
+protocol FeedImageCellControllerDelegate {
+	func didRequestImage()
+	func didCancelImageRequest()
+}
+
+final class FeedImageCellController: FeedImageView {
+	private let delegate: FeedImageCellControllerDelegate
+	private lazy var cell = FeedImageCell()
 	
-	init(viewModel: FeedImageViewModel<UIImage>) {
-		self.viewModel = viewModel
+	init(delegate: FeedImageCellControllerDelegate) {
+		self.delegate = delegate
 	}
 	
 	func view() -> UITableViewCell {
-		let cell = binded(FeedImageCell())
-		viewModel.loadImageData()
+		delegate.didRequestImage()
 		return cell
 	}
 	
 	func preload() {
-		viewModel.loadImageData()
+		delegate.didRequestImage()
 	}
 	
 	func cancelLoad() {
-		viewModel.cancelImageDataLoad()
+		delegate.didCancelImageRequest()
 	}
 	
-	private func binded(_ cell: FeedImageCell) -> FeedImageCell {
+	func display(_ viewModel: FeedImageViewModel<UIImage>) {
 		cell.locationContainer.isHidden = !viewModel.hasLocation
 		cell.locationLabel.text = viewModel.location
 		cell.descriptionLabel.text = viewModel.description
-		cell.onRetry = viewModel.loadImageData
-		
-		viewModel.onImageLoad = { [weak cell] image in
-			cell?.feedImageView.image = image
-		}
-		
-		viewModel.onImageLoadingStateChange = { [weak cell] isLoading in
-			cell?.feedImageContainer.isShimmering = isLoading
-		}
-		
-		viewModel.onShouldRetryImageLoadStateChange = { [weak cell] shouldRetry in
-			cell?.feedImageRetryButton.isHidden = !shouldRetry
-		}
-		
-		return cell
+		cell.feedImageView.image = viewModel.image
+		cell.feedImageContainer.isShimmering = viewModel.isLoading
+		cell.feedImageRetryButton.isHidden = !viewModel.shouldRetry
+		cell.onRetry = delegate.didRequestImage
 	}
 }

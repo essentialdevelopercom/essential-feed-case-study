8e9288ee21aa7c08c5cf7581e37301fe3fbb109d
Caio Zullo <caiozullo@gmail.com>
2019-08-15 12:59:48 +0100

    Add UITableView helper to dequeue typed cells by class name

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -18,10 +18,9 @@ final class FeedImageCellController: FeedImageView {
 	}
 	
 	func view(in tableView: UITableView) -> UITableViewCell {
-		let cell = tableView.dequeueReusableCell(withIdentifier: "FeedImageCell") as! FeedImageCell
-		self.cell = cell
+		cell = tableView.dequeueReusableCell()
 		delegate.didRequestImage()
-		return cell
+		return cell!
 	}
 	
 	func preload() {

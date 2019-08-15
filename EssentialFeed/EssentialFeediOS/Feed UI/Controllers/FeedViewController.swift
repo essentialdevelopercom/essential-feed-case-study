0d34953e52266d530ae2133f3072e0180f21fc28
Caio Zullo <caiozullo@gmail.com>
2019-08-15 12:26:55 +0100

    Move tableView.prefetchDataSource setup to the storyboard

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -18,7 +18,6 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
 	public override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		tableView.prefetchDataSource = self
 		refresh()
 	}
 	

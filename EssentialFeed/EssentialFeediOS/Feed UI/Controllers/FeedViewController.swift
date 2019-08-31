b62d6ca2d2c7125ed6f31f1bfa365dd627c9615d
Caio Zullo <caiozullo@gmail.com>
2019-08-31 13:22:25 +0100

    Dispatch background feed completion to main thread before updating the UI since UIKit is not thread safe.

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -12,7 +12,15 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
 	var delegate: FeedViewControllerDelegate?
 	
 	var tableModel = [FeedImageCellController]() {
-		didSet { tableView.reloadData() }
+		didSet {
+			if Thread.isMainThread {
+				tableView.reloadData()
+			} else {
+				DispatchQueue.main.async { [weak self] in
+					self?.tableView.reloadData()
+				}
+			}
+		}
 	}
 
 	public override func viewDidLoad() {
@@ -26,6 +34,10 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
 	}
 	
 	func display(_ viewModel: FeedLoadingViewModel) {
+		guard Thread.isMainThread else {
+			return DispatchQueue.main.async { [weak self] in self?.display(viewModel) }
+		}
+		
 		if viewModel.isLoading {
 			refreshControl?.beginRefreshing()
 		} else {
--- EssentialFeed/EssentialFeediOSTests/Feed UI/FeedUIIntegrationTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/FeedUIIntegrationTests.swift
@@ -254,6 +254,18 @@ final class FeedUIIntegrationTests: XCTestCase {
 		XCTAssertNil(view?.renderedImage, "Expected no rendered image when an image load finishes after the view is not visible anymore")
 	}
 	
+	func test_loadFeedCompletion_dispatchesFromBackgroundToMainThread() {
+		let (sut, loader) = makeSUT()
+		sut.loadViewIfNeeded()
+
+		let exp = expectation(description: "Wait for background queue")
+		DispatchQueue.global().async {
+			loader.completeFeedLoading(at: 0)
+			exp.fulfill()
+		}
+		wait(for: [exp], timeout: 1.0)
+	}
+	
 	// MARK: - Helpers
 	
 	private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {

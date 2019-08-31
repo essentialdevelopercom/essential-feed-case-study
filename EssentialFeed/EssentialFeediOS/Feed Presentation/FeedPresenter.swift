555a2ebf4794835f6f175b193646cacd56a2655c
Caio Zullo <caiozullo@gmail.com>
2019-08-31 13:41:42 +0100

    Move main thread dispatch to the Composition layer with a Decorator.

--- EssentialFeed/EssentialFeediOS/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedPresenter.swift
@@ -30,27 +30,15 @@ final class FeedPresenter {
 	}
 
 	func didStartLoadingFeed() {
-		guard Thread.isMainThread else {
-			return DispatchQueue.main.async { [weak self] in self?.didStartLoadingFeed() }
-		}
-
 		loadingView.display(FeedLoadingViewModel(isLoading: true))
 	}
 	
 	func didFinishLoadingFeed(with feed: [FeedImage]) {
-		guard Thread.isMainThread else {
-			return DispatchQueue.main.async { [weak self] in self?.didFinishLoadingFeed(with: feed) }
-		}
-
 		feedView.display(FeedViewModel(feed: feed))
 		loadingView.display(FeedLoadingViewModel(isLoading: false))
 	}
 	
 	func didFinishLoadingFeed(with error: Error) {
-		guard Thread.isMainThread else {
-			return DispatchQueue.main.async { [weak self] in self?.didFinishLoadingFeed(with: error) }
-		}
-
 		loadingView.display(FeedLoadingViewModel(isLoading: false))
 	}
 }
--- EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -9,7 +9,8 @@ public final class FeedUIComposer {
 	private init() {}
 	
 	public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
-		let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader)
+		let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader:
+			MainQueueDispatchDecorator(decoratee: feedLoader))
 		
 		let feedController = FeedViewController.makeWith(
 			delegate: presentationAdapter,
@@ -20,6 +21,26 @@ public final class FeedUIComposer {
 			loadingView: WeakRefVirtualProxy(feedController))
 		
 		return feedController
+	}
+}
+
+private final class MainQueueDispatchDecorator: FeedLoader {
+	private let decoratee: FeedLoader
+	
+	init(decoratee: FeedLoader) {
+		self.decoratee = decoratee
+	}
+	
+	func load(completion: @escaping (FeedLoader.Result) -> Void) {
+		decoratee.load { result in
+			if Thread.isMainThread {
+				completion(result)
+			} else {
+				DispatchQueue.main.async {
+					completion(result)
+				}
+			}
+		}
 	}
 }
 

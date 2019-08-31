391f5e3ee3225368cdaf5741de4036a5ccacef4c
Caio Zullo <caiozullo@gmail.com>
2019-08-31 13:57:50 +0100

    Move `FeedLoaderPresentationAdapter` to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedLoaderPresentationAdapter.swift
@@ -0,0 +1,28 @@
+//
+//  Copyright © 2019 Essential Developer. All rights reserved.
+//
+
+import EssentialFeed
+
+final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
+	private let feedLoader: FeedLoader
+	var presenter: FeedPresenter?
+	
+	init(feedLoader: FeedLoader) {
+		self.feedLoader = feedLoader
+	}
+	
+	func didRequestFeedRefresh() {
+		presenter?.didStartLoadingFeed()
+		
+		feedLoader.load { [weak self] result in
+			switch result {
+			case let .success(feed):
+				self?.presenter?.didFinishLoadingFeed(with: feed)
+				
+			case let .failure(error):
+				self?.presenter?.didFinishLoadingFeed(with: error)
+			}
+		}
+	}
+}
--- EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -59,26 +59,3 @@ private final class FeedViewAdapter: FeedView {
 		}
 	}
 }
-
-private final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
-	private let feedLoader: FeedLoader
-	var presenter: FeedPresenter?
-	
-	init(feedLoader: FeedLoader) {
-		self.feedLoader = feedLoader
-	}
-	
-	func didRequestFeedRefresh() {
-		presenter?.didStartLoadingFeed()
-		
-		feedLoader.load { [weak self] result in
-			switch result {
-			case let .success(feed):
-				self?.presenter?.didFinishLoadingFeed(with: feed)
-				
-			case let .failure(error):
-				self?.presenter?.didFinishLoadingFeed(with: error)
-			}
-		}
-	}
-}

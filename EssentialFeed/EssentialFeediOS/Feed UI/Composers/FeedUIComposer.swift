f9e61520b32d21bfc545ceb22850031f2b6ac339
Caio Zullo <caiozullo@gmail.com>
2019-08-31 13:47:58 +0100

    Make `MainQueueDispatchDecorator` generic

--- EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -24,22 +24,26 @@ public final class FeedUIComposer {
 	}
 }
 
-private final class MainQueueDispatchDecorator: FeedLoader {
-	private let decoratee: FeedLoader
+private final class MainQueueDispatchDecorator<T> {
+	private let decoratee: T
 	
-	init(decoratee: FeedLoader) {
+	init(decoratee: T) {
 		self.decoratee = decoratee
 	}
 	
+	func dispatch(completion: @escaping () -> Void) {
+		guard Thread.isMainThread else {
+			return DispatchQueue.main.async(execute: completion)
+		}
+		
+		completion()
+	}
+}
+
+extension MainQueueDispatchDecorator: FeedLoader where T == FeedLoader {
 	func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		decoratee.load { result in
-			if Thread.isMainThread {
-				completion(result)
-			} else {
-				DispatchQueue.main.async {
-					completion(result)
-				}
-			}
+		decoratee.load { [weak self] result in
+			self?.dispatch { completion(result) }
 		}
 	}
 }

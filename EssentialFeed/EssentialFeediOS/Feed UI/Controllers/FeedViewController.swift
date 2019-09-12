0ffb7a8a481d7acec45332bc2cd82a6c6eb23d4a
Caio Zullo <caiozullo@gmail.com>
2019-09-12 16:47:53 +0100

    Does not render load feed error on load

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -8,9 +8,14 @@ protocol FeedViewControllerDelegate {
 	func didRequestFeedRefresh()
 }
 
+public final class ErrorView: UIView {
+	public var message: String?
+}
+
 public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, FeedLoadingView {
 	var delegate: FeedViewControllerDelegate?
-	
+	public let errorView = ErrorView()
+
 	var tableModel = [FeedImageCellController]() {
 		didSet { tableView.reloadData() }
 	}
--- EssentialFeed/EssentialFeediOSTests/Feed UI/FeedUIIntegrationTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/FeedUIIntegrationTests.swift
@@ -77,6 +77,14 @@ final class FeedUIIntegrationTests: XCTestCase {
 		loader.completeFeedLoadingWithError(at: 1)
 		assertThat(sut, isRendering: [image0])
 	}
+	
+	func test_errorView_doesNotRenderErrorOnLoad() {
+		let (sut, _) = makeSUT()
+		
+		sut.loadViewIfNeeded()
+		
+		XCTAssertEqual(sut.errorMessage, nil)
+	}
 
 	func test_feedImageView_loadsImageURLWhenVisible() {
 		let image0 = makeImage(url: URL(string: "http://url-0.com")!)

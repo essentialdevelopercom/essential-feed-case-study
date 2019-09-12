8051fd8dc765b1995fe79f9572a2d783f2435a27
Caio Zullo <caiozullo@gmail.com>
2019-09-12 16:53:45 +0100

    Move error view configuration to storyboard

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -14,7 +14,7 @@ public final class ErrorView: UIView {
 
 public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, FeedLoadingView, FeedErrorView {
 	var delegate: FeedViewControllerDelegate?
-	public let errorView = ErrorView()
+	@IBOutlet private(set) public var errorView: ErrorView?
 
 	var tableModel = [FeedImageCellController]() {
 		didSet { tableView.reloadData() }
@@ -39,7 +39,7 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
 	}
 	
 	func display(_ viewModel: FeedErrorViewModel) {
-		errorView.message = viewModel.message
+		errorView?.message = viewModel.message
 	}
 
 	public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
--- EssentialFeed/EssentialFeediOSTests/Feed UI/Helpers/FeedViewController+TestHelpers.swift
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/Helpers/FeedViewController+TestHelpers.swift
@@ -41,7 +41,7 @@ extension FeedViewController {
 	}
 	
 	var errorMessage: String? {
-		return errorView.message
+		return errorView?.message
 	}
 
 	var isShowingLoadingIndicator: Bool {

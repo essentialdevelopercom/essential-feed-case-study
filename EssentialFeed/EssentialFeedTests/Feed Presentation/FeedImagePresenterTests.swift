b8b7840b048c2d8a232d22e12708ad5c871fe460
Caio Zullo <caiozullo@gmail.com>
2019-09-13 17:11:00 +0100

    `FeedImagePresenter` displays retry when image transformation fails on `didFinishLoadingImageData`

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -23,9 +23,11 @@ protocol FeedImageView {
 
 class FeedImagePresenter {
 	private let view: FeedImageView
+	private let imageTransformer: (Data) -> Any?
 
-	init(view: FeedImageView) {
+	init(view: FeedImageView, imageTransformer: @escaping (Data) -> Any?) {
 		self.view = view
+		self.imageTransformer = imageTransformer
 	}
 	
 	func didStartLoadingImageData(for model: FeedImage) {
@@ -35,6 +37,15 @@ class FeedImagePresenter {
 			image: nil,
 			isLoading: true,
 			shouldRetry: false))
+	}
+	
+	func didFinishLoadingImageData(with data: Data, for model: FeedImage) {
+		view.display(FeedImageViewModel(
+			description: model.description,
+			location: model.location,
+			image: imageTransformer(data),
+			isLoading: false,
+			shouldRetry: true))
 	}
 }
 
@@ -61,11 +72,31 @@ class FeedImagePresenterTests: XCTestCase {
 		XCTAssertNil(message?.image)
 	}
 	
+	func test_didFinishLoadingImageData_displaysRetryOnFailedImageTransformation() {
+		let (sut, view) = makeSUT(imageTransformer: { _ in nil })
+		let image = uniqueImage()
+		let data = Data()
+		
+		sut.didFinishLoadingImageData(with: data, for: image)
+		
+		let message = view.messages.first
+		XCTAssertEqual(view.messages.count, 1)
+		XCTAssertEqual(message?.description, image.description)
+		XCTAssertEqual(message?.location, image.location)
+		XCTAssertEqual(message?.isLoading, false)
+		XCTAssertEqual(message?.shouldRetry, true)
+		XCTAssertNil(message?.image)
+	}
+	
 	// MARK: - Helpers
 	
-	private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedImagePresenter, view: ViewSpy) {
+	private func makeSUT(
+		imageTransformer: @escaping (Data) -> Any? = { _ in nil },
+		file: StaticString = #file,
+		line: UInt = #line
+	) -> (sut: FeedImagePresenter, view: ViewSpy) {
 		let view = ViewSpy()
-		let sut = FeedImagePresenter(view: view)
+		let sut = FeedImagePresenter(view: view, imageTransformer: imageTransformer)
 		trackForMemoryLeaks(view, file: file, line: line)
 		trackForMemoryLeaks(sut, file: file, line: line)
 		return (sut, view)

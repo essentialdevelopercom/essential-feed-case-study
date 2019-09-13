733e00fbfd5b83f224745a7ed0b4de062193017e
Caio Zullo <caiozullo@gmail.com>
2019-09-13 17:03:27 +0100

    `FeedImagePresenter` displays loading image on `didStartLoadingImageData`

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -3,10 +3,38 @@
 //
 
 import XCTest
+import EssentialFeed
+
+struct FeedImageViewModel {
+	let description: String?
+	let location: String?
+	let image: Any?
+	let isLoading: Bool
+	let shouldRetry: Bool
+	
+	var hasLocation: Bool {
+		return location != nil
+	}
+}
+
+protocol FeedImageView {
+	func display(_ model: FeedImageViewModel)
+}
 
 class FeedImagePresenter {
-	init(view: Any) {
-		
+	private let view: FeedImageView
+
+	init(view: FeedImageView) {
+		self.view = view
+	}
+	
+	func didStartLoadingImageData(for model: FeedImage) {
+		view.display(FeedImageViewModel(
+			description: model.description,
+			location: model.location,
+			image: nil,
+			isLoading: true,
+			shouldRetry: false))
 	}
 }
 
@@ -16,6 +44,21 @@ class FeedImagePresenterTests: XCTestCase {
 		let (_, view) = makeSUT()
 		
 		XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
+	}
+	
+	func test_didStartLoadingImageData_displaysLoadingImage() {
+		let (sut, view) = makeSUT()
+		let image = uniqueImage()
+		
+		sut.didStartLoadingImageData(for: image)
+		
+		let message = view.messages.first
+		XCTAssertEqual(view.messages.count, 1)
+		XCTAssertEqual(message?.description, image.description)
+		XCTAssertEqual(message?.location, image.location)
+		XCTAssertEqual(message?.isLoading, true)
+		XCTAssertEqual(message?.shouldRetry, false)
+		XCTAssertNil(message?.image)
 	}
 	
 	// MARK: - Helpers
@@ -28,8 +71,12 @@ class FeedImagePresenterTests: XCTestCase {
 		return (sut, view)
 	}
 	
-	private class ViewSpy {
-		let messages = [Any]()
+	private class ViewSpy: FeedImageView {
+		private(set) var messages = [FeedImageViewModel]()
+		
+		func display(_ model: FeedImageViewModel) {
+			messages.append(model)
+		}
 	}
 
 }

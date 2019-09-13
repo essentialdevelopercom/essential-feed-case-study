4c8e1b9d0d7824f7179d8226280fb2f42e9f1e63
Caio Zullo <caiozullo@gmail.com>
2019-09-13 17:36:35 +0100

    Move `FeedImageViewModel` to separate file

--- EssentialFeed/EssentialFeed/Feed Presentation/FeedImagePresenter.swift
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedImagePresenter.swift
@@ -4,18 +4,6 @@
 
 import Foundation
 
-public struct FeedImageViewModel<Image> {
-	public let description: String?
-	public let location: String?
-	public let image: Image?
-	public let isLoading: Bool
-	public let shouldRetry: Bool
-	
-	public var hasLocation: Bool {
-		return location != nil
-	}
-}
-
 public protocol FeedImageView {
 	associatedtype Image
 	

59b9936336af905931e4dcb1c30b8361ce7b789a
Caio Zullo <caiozullo@gmail.com>
2019-09-23 17:07:22 +0100

    `RemoteFeedImageDataLoader.loadImageDataFromURL` delivers non-empty received data on 200 HTTP response

--- EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -19,7 +19,12 @@ class RemoteFeedImageDataLoader {
 	func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case .success: completion(.failure(Error.invalidData))
+			case let .success(data, response):
+				if response.statusCode == 200, !data.isEmpty {
+					completion(.success(data))
+				} else {
+					completion(.failure(Error.invalidData))
+				}
 			case let .failure(error): completion(.failure(error))
 			}
 		}
@@ -80,6 +85,15 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
 		expect(sut, toCompleteWith: failure(.invalidData), when: {
 			let emptyData = Data()
 			client.complete(withStatusCode: 200, data: emptyData)
+		})
+	}
+	
+	func test_loadImageDataFromURL_deliversReceivedNonEmptyDataOn200HTTPResponse() {
+		let (sut, client) = makeSUT()
+		let nonEmptyData = Data("non-empty data".utf8)
+		
+		expect(sut, toCompleteWith: .success(nonEmptyData), when: {
+			client.complete(withStatusCode: 200, data: nonEmptyData)
 		})
 	}
 

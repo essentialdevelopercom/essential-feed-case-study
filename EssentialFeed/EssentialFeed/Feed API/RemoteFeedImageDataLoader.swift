09b2b842017101be6f74fadc4d08ccf31833cd04
Caio Zullo <caiozullo@gmail.com>
2019-09-23 18:44:19 +0100

    `RemoteFeedImageDataLoader.loadImageDataFromURL` delivers connectivity error on HTTP client error

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
@@ -12,6 +12,7 @@ public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
 	}
 	
 	public enum Error: Swift.Error {
+		case connectivity
 		case invalidData
 	}
 	
@@ -51,7 +52,9 @@ public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
 				} else {
 					task.complete(with: .failure(Error.invalidData))
 				}
-			case let .failure(error): task.complete(with: .failure(error))
+			
+			case .failure:
+				task.complete(with: .failure(Error.connectivity))
 			}
 		}
 		return task
--- EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -32,11 +32,11 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
 		XCTAssertEqual(client.requestedURLs, [url, url])
 	}
 	
-	func test_loadImageDataFromURL_deliversErrorOnClientError() {
+	func test_loadImageDataFromURL_deliversConnectivityErrorOnClientError() {
 		let (sut, client) = makeSUT()
 		let clientError = NSError(domain: "a client error", code: 0)
 
-		expect(sut, toCompleteWith: .failure(clientError), when: {
+		expect(sut, toCompleteWith: failure(.connectivity), when: {
 			client.complete(with: clientError)
 		})
 	}

f398ffea518a5c945237a15dc413f65098ded49d
Caio Zullo <caiozullo@gmail.com>
2019-09-23 17:03:21 +0100

    `RemoteFeedImageDataLoader.loadImageDataFromURL` delivers invalid data error on 200 HTTP response with empty data

--- EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -73,6 +73,15 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
 			})
 		}
 	}
+	
+	func test_loadImageDataFromURL_deliversInvalidDataErrorOn200HTTPResponseWithEmptyData() {
+		let (sut, client) = makeSUT()
+		
+		expect(sut, toCompleteWith: failure(.invalidData), when: {
+			let emptyData = Data()
+			client.complete(withStatusCode: 200, data: emptyData)
+		})
+	}
 
 	private func makeSUT(url: URL = anyURL(), file: StaticString = #file, line: UInt = #line) -> (sut: RemoteFeedImageDataLoader, client: HTTPClientSpy) {
 		let client = HTTPClientSpy()

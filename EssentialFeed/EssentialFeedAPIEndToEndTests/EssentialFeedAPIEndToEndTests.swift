99550d86d3f9cb26ce419b8aa8c5d59a717ccfd2
Caio Zullo <caiozullo@gmail.com>
2019-09-24 09:54:05 +0100

    Add `RemoteFeedImageDataLoader` end-to-end test to guarantee both the client (app) and server (backend) respects the API contract, so we can fetch image data from remote.

--- EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -28,6 +28,19 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
 		}
 	}
 	
+	func test_endToEndTestServerGETFeedImageDataResult_matchesFixedTestAccountData() {
+		switch getFeedImageDataResult() {
+		case let .success(data)?:
+			XCTAssertFalse(data.isEmpty, "Expected non-empty image data")
+			
+		case let .failure(error)?:
+			XCTFail("Expected successful image data result, got \(error) instead")
+			
+		default:
+			XCTFail("Expected successful image data result, got no result instead")
+		}
+	}
+	
 	// MARK: - Helpers
 	
 	private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> FeedLoader.Result? {
@@ -41,6 +54,25 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
 		
 		var receivedResult: FeedLoader.Result?
 		loader.load { result in
+			receivedResult = result
+			exp.fulfill()
+		}
+		wait(for: [exp], timeout: 5.0)
+		
+		return receivedResult
+	}
+	
+	private func getFeedImageDataResult(file: StaticString = #file, line: UInt = #line) -> FeedImageDataLoader.Result? {
+		let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed/73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")!
+		let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+		let loader = RemoteFeedImageDataLoader(client: client)
+		trackForMemoryLeaks(client, file: file, line: line)
+		trackForMemoryLeaks(loader, file: file, line: line)
+		
+		let exp = expectation(description: "Wait for load completion")
+		
+		var receivedResult: FeedImageDataLoader.Result?
+		_ = loader.loadImageData(from: testServerURL) { result in
 			receivedResult = result
 			exp.fulfill()
 		}

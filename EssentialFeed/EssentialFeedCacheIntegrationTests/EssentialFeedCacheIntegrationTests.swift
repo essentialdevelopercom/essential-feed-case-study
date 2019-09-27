e207341635ce377d6c44fcfb3264a82d512d9e76
Caio Zullo <caiozullo@gmail.com>
2019-09-27 15:36:30 +0100

    Separate test scopes with better naming

--- EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -19,33 +19,35 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
 		undoStoreSideEffects()
 	}
 	
-	func test_load_deliversNoItemsOnEmptyCache() {
-		let sut = makeFeedLoader()
+	// MARK: - LocalFeedLoader Tests
 
-		expect(sut, toLoad: [])
+	func test_loadFeed_deliversNoItemsOnEmptyCache() {
+		let feedLoader = makeFeedLoader()
+
+		expect(feedLoader, toLoad: [])
 	}
 	
-	func test_load_deliversItemsSavedOnASeparateInstance() {
-		let sutToPerformSave = makeFeedLoader()
-		let sutToPerformLoad = makeFeedLoader()
+	func test_loadFeed_deliversItemsSavedOnASeparateInstance() {
+		let feedLoaderToPerformSave = makeFeedLoader()
+		let feedLoaderToPerformLoad = makeFeedLoader()
 		let feed = uniqueImageFeed().models
 		
-		save(feed, with: sutToPerformSave)
+		save(feed, with: feedLoaderToPerformSave)
 
-		expect(sutToPerformLoad, toLoad: feed)
+		expect(feedLoaderToPerformLoad, toLoad: feed)
 	}
 	
-	func test_save_overridesItemsSavedOnASeparateInstance() {
-		let sutToPerformFirstSave = makeFeedLoader()
-		let sutToPerformLastSave = makeFeedLoader()
-		let sutToPerformLoad = makeFeedLoader()
+	func test_saveFeed_overridesItemsSavedOnASeparateInstance() {
+		let feedLoaderToPerformFirstSave = makeFeedLoader()
+		let feedLoaderToPerformLastSave = makeFeedLoader()
+		let feedLoaderToPerformLoad = makeFeedLoader()
 		let firstFeed = uniqueImageFeed().models
 		let latestFeed = uniqueImageFeed().models
 		
-		save(firstFeed, with: sutToPerformFirstSave)
-		save(latestFeed, with: sutToPerformLastSave)
+		save(firstFeed, with: feedLoaderToPerformFirstSave)
+		save(latestFeed, with: feedLoaderToPerformLastSave)
 
-		expect(sutToPerformLoad, toLoad: latestFeed)
+		expect(feedLoaderToPerformLoad, toLoad: latestFeed)
 	}
 	
 	// MARK: - LocalFeedImageDataLoader Tests

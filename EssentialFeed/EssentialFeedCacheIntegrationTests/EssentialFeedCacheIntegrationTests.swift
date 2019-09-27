6f55c9ff0c9244d971c1453f2af8dc3b9808d911
Caio Zullo <caiozullo@gmail.com>
2019-09-27 15:40:01 +0100

    `LocalFeedImageDataLoader` in integration with the `CoreDataFeedStore` overrides items saved by separate instances, proving we correctly manage the data models on disk.

--- EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -65,6 +65,22 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
 		expect(imageLoaderToPerformLoad, toLoad: dataToSave, for: image.url)
 	}
 	
+	func test_saveImageData_overridesSavedImageDataOnASeparateInstance() {
+		let imageLoaderToPerformFirstSave = makeImageLoader()
+		let imageLoaderToPerformLastSave = makeImageLoader()
+		let imageLoaderToPerformLoad = makeImageLoader()
+		let feedLoader = makeFeedLoader()
+		let image = uniqueImage()
+		let firstImageData = Data("first".utf8)
+		let lastImageData = Data("last".utf8)
+		
+		save([image], with: feedLoader)
+		save(firstImageData, for: image.url, with: imageLoaderToPerformFirstSave)
+		save(lastImageData, for: image.url, with: imageLoaderToPerformLastSave)
+
+		expect(imageLoaderToPerformLoad, toLoad: lastImageData, for: image.url)
+	}
+
 	// MARK: - Helpers
 	
 	private func makeFeedLoader(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {

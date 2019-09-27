2996516f750329fc3ed43b68c9da6e2f13dfa242
Caio Zullo <caiozullo@gmail.com>
2019-09-27 15:52:53 +0100

    `LocalFeedLoader` in integration with the `CoreDataFeedStore` deletes items saved in a distant past by separate instances when validating cache, proving we correctly manage the data models on disk.

--- EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -60,6 +60,17 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
 		
 		expect(feedLoaderToPerformSave, toLoad: feed)
 	}
+	
+	func test_validateFeedCache_deletesFeedSavedInADistantPast() {
+		let feedLoaderToPerformSave = makeFeedLoader(currentDate: .distantPast)
+		let feedLoaderToPerformValidation = makeFeedLoader(currentDate: Date())
+		let feed = uniqueImageFeed().models
+		
+		save(feed, with: feedLoaderToPerformSave)
+		validateCache(with: feedLoaderToPerformValidation)
+
+		expect(feedLoaderToPerformSave, toLoad: [])
+	}
 
 	// MARK: - LocalFeedImageDataLoader Tests
 	
@@ -94,10 +105,10 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
 
 	// MARK: - Helpers
 	
-	private func makeFeedLoader(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
+	private func makeFeedLoader(currentDate: Date = Date(), file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
 		let storeURL = testSpecificStoreURL()
 		let store = try! CoreDataFeedStore(storeURL: storeURL)
-		let sut = LocalFeedLoader(store: store, currentDate: Date.init)
+		let sut = LocalFeedLoader(store: store, currentDate: { currentDate })
 		trackForMemoryLeaks(store, file: file, line: line)
 		trackForMemoryLeaks(sut, file: file, line: line)
 		return sut

6ff50e43f51415f00054770c2bdf8be1e84a2740
Caio Zullo <caiozullo@gmail.com>
2019-10-18 13:11:02 +0100

    `FeedImageDataLoaderWithFallbackComposite.loadImageData` delivers fallback data on fallback loader success

--- EssentialApp/EssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -30,7 +30,7 @@ class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
 				completion(result)
 				
 			case .failure:
-				task.wrapped = self?.fallback.loadImageData(from: url) { _ in }
+				task.wrapped = self?.fallback.loadImageData(from: url, completion: completion)
 			}
 
 		}
@@ -98,6 +98,16 @@ class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
 		
 		expect(sut, toCompleteWith: .success(primaryData), when: {
 			primaryLoader.complete(with: primaryData)
+		})
+	}
+	
+	func test_loadImageData_deliversFallbackDataOnFallbackLoaderSuccess() {
+		let fallbackData = anyData()
+		let (sut, primaryLoader, fallbackLoader) = makeSUT()
+		
+		expect(sut, toCompleteWith: .success(fallbackData), when: {
+			primaryLoader.complete(with: anyNSError())
+			fallbackLoader.complete(with: fallbackData)
 		})
 	}
 

bcbd54c4f51778298d4ebf0dace25633d5963870
Caio Zullo <caiozullo@gmail.com>
2019-10-18 12:43:09 +0100

    `FeedImageDataLoaderWithFallbackComposite.loadImageData` loads from primary loader first

--- EssentialApp/EssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -6,17 +6,20 @@ import XCTest
 import EssentialFeed
 
 class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
+	private let primary: FeedImageDataLoader
+	
+	init(primary: FeedImageDataLoader, fallback: FeedImageDataLoader) {
+		self.primary = primary
+	}
+	
 	private class Task: FeedImageDataLoaderTask {
 		func cancel() {
 			
 		}
 	}
-	
-	init(primary: FeedImageDataLoader, fallback: FeedImageDataLoader) {
-		
-	}
-	
+
 	func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+		_ = primary.loadImageData(from: url) { _ in }
 		return Task()
 	}
 }
@@ -31,9 +34,25 @@ class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
 		XCTAssertTrue(primaryLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the primary loader")
 		XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
 	}
+	
+	func test_loadImageData_loadsFromPrimaryLoaderFirst() {
+		let url = anyURL()
+		let primaryLoader = LoaderSpy()
+		let fallbackLoader = LoaderSpy()
+		let sut = FeedImageDataLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+
+		_ = sut.loadImageData(from: url) { _ in }
+		
+		XCTAssertEqual(primaryLoader.loadedURLs, [url], "Expected to load URL from primary loader")
+		XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
+	}
 		
 	// MARK: - Helpers
 	
+	private func anyURL() -> URL {
+		return URL(string: "http://a-url.com")!
+	}
+
 	private class LoaderSpy: FeedImageDataLoader {
 		private var messages = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
 

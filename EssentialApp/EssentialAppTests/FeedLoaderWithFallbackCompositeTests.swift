d78395f9c4bf34811fd39badc79634d8291222fc
Caio Zullo <caiozullo@gmail.com>
2019-10-18 17:40:08 +0100

    Extract system under test (SUT) creation into a factory method

--- EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -22,10 +22,8 @@ class FeedLoaderWithFallbackCompositeTests: XCTestCase {
 	func test_load_deliversPrimaryFeedOnPrimaryLoaderSuccess() {
 		let primaryFeed = uniqueFeed()
 		let fallbackFeed = uniqueFeed()
-		let primaryLoader = LoaderStub(result: .success(primaryFeed))
-		let fallbackLoader = LoaderStub(result: .success(fallbackFeed))
-		let sut = FeedLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
-		
+		let sut = makeSUT(primaryResult: .success(primaryFeed), fallbackResult: .success(fallbackFeed))
+
 		let exp = expectation(description: "Wait for load completion")
 		sut.load { result in
 			switch result {
@@ -39,6 +37,24 @@ class FeedLoaderWithFallbackCompositeTests: XCTestCase {
 			exp.fulfill()
 		}
 		wait(for: [exp], timeout: 1)
+	}
+	
+	// MARK: - Helpers
+	
+	private func makeSUT(primaryResult: FeedLoader.Result, fallbackResult: FeedLoader.Result, file: StaticString = #file, line: UInt = #line) -> FeedLoader {
+		let primaryLoader = LoaderStub(result: primaryResult)
+		let fallbackLoader = LoaderStub(result: fallbackResult)
+		let sut = FeedLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+		trackForMemoryLeaks(primaryLoader, file: file, line: line)
+		trackForMemoryLeaks(fallbackLoader, file: file, line: line)
+		trackForMemoryLeaks(sut, file: file, line: line)
+		return sut
+	}
+
+	private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
+		addTeardownBlock { [weak instance] in
+			XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+		}
 	}
 	
 	private func uniqueFeed() -> [FeedImage] {

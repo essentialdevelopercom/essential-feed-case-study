99ffefd6cac4463fa0a7744e4ec522ccde8ee9bc
Caio Zullo <caiozullo@gmail.com>
2019-10-18 17:45:05 +0100

    `FeedLoaderWithFallbackComposite.load` delivers fallback feed on primary loader failure

--- EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -7,13 +7,23 @@ import EssentialFeed
 
 class FeedLoaderWithFallbackComposite: FeedLoader {
 	private let primary: FeedLoader
+	private let fallback: FeedLoader
 
 	init(primary: FeedLoader, fallback: FeedLoader) {
 		self.primary = primary
+		self.fallback = fallback
 	}
 	
 	func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		primary.load(completion: completion)
+		primary.load { [weak self] result in
+			switch result {
+			case .success:
+				completion(result)
+				
+			case .failure:
+				self?.fallback.load(completion: completion)
+			}
+		}
 	}
 }
 
@@ -39,6 +49,27 @@ class FeedLoaderWithFallbackCompositeTests: XCTestCase {
 		wait(for: [exp], timeout: 1)
 	}
 	
+	func test_load_deliversFallbackFeedOnPrimaryLoaderFailure() {
+		let fallbackFeed = uniqueFeed()
+		let sut = makeSUT(primaryResult: .failure(anyNSError()), fallbackResult: .success(fallbackFeed))
+
+		let exp = expectation(description: "Wait for load completion")
+		
+		sut.load { result in
+			switch result {
+			case let .success(receivedFeed):
+				XCTAssertEqual(receivedFeed, fallbackFeed)
+
+			case .failure:
+				XCTFail("Expected successful load feed result, got \(result) instead")
+			}
+			
+			exp.fulfill()
+		}
+		
+		wait(for: [exp], timeout: 1.0)
+	}
+	
 	// MARK: - Helpers
 	
 	private func makeSUT(primaryResult: FeedLoader.Result, fallbackResult: FeedLoader.Result, file: StaticString = #file, line: UInt = #line) -> FeedLoader {
@@ -57,6 +88,10 @@ class FeedLoaderWithFallbackCompositeTests: XCTestCase {
 		}
 	}
 	
+	private func anyNSError() -> NSError {
+		return NSError(domain: "any error", code: 0)
+	}
+
 	private func uniqueFeed() -> [FeedImage] {
 		return [FeedImage(id: UUID(), description: "any", location: "any", url: URL(string: "http://any-url.com")!)]
 	}

be0d1b2de47a29c15377cd98f302c5a044e8b7ed
Caio Zullo <caiozullo@gmail.com>
2019-10-25 16:00:31 +0100

    `FeedImageDataLoaderCacheDecorator.loadImageData` does not cache image data on loader failure

--- EssentialApp/EssentialAppTests/FeedImageDataLoaderCacheDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderCacheDecoratorTests.swift
@@ -23,8 +23,10 @@ class FeedImageDataLoaderCacheDecorator: FeedImageDataLoader {
 	
 	func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
 		return decoratee.loadImageData(from: url) { [weak self] result in
-			self?.cache.save((try? result.get()) ?? Data(), for: url) { _ in }
-			completion(result)
+			completion(result.map { data in
+				self?.cache.save(data, for: url) { _ in }
+				return data
+			})
 		}
 	}
 }
@@ -83,6 +85,17 @@ class FeedImageDataLoaderCacheDecoratorTests: XCTestCase, FeedImageDataLoaderTes
 		loader.complete(with: imageData)
 
 		XCTAssertEqual(cache.messages, [.save(data: imageData, for: url)], "Expected to cache loaded image data on success")
+	}
+	
+	func test_loadImageData_doesNotCacheDataOnLoaderFailure() {
+		let cache = CacheSpy()
+		let url = anyURL()
+		let (sut, loader) = makeSUT(cache: cache)
+
+		_ = sut.loadImageData(from: url) { _ in }
+		loader.complete(with: anyNSError())
+
+		XCTAssertTrue(cache.messages.isEmpty, "Expected not to cache image data on load error")
 	}
 
 	// MARK: - Helpers

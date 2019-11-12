9f7a5360bb980c8dede5b60e3b05b1e184cf14d5
Caio Zullo <caiozullo@gmail.com>
2019-11-12 14:42:28 +0000

    Displays remote feed on launch when customer has connectivity

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -3,6 +3,8 @@
 //
 
 import UIKit
+import EssentialFeed
+import EssentialFeediOS
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	var window: UIWindow?
@@ -10,6 +12,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 		guard let _ = (scene as? UIWindowScene) else { return }
 		
+		let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
 		
+		let remoteClient = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+		let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: remoteClient)
+		let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
+				
+		window?.rootViewController = FeedUIComposer.feedComposedWith(
+			feedLoader: remoteFeedLoader,
+			imageLoader: remoteImageLoader)
 	}
 }
--- EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -6,6 +6,13 @@ import XCTest
 
 class EssentialAppUIAcceptanceTests: XCTestCase {
 	
-	
+	func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
+		let app = XCUIApplication()
+		
+		app.launch()
+		
+		XCTAssertEqual(app.cells.count, 22)
+		XCTAssertEqual(app.cells.firstMatch.images.count, 1)
+	}
 	
 }

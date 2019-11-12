e7fb26075dc217729ea3b42f4e738bd1c8f92fd5
Caio Zullo <caiozullo@gmail.com>
2019-11-12 15:06:17 +0000

    Displays empty feed when customer has no connectivity and no cache

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -23,6 +23,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 			.defaultDirectoryURL()
 			.appendingPathComponent("feed-store.sqlite")
 
+		if CommandLine.arguments.contains("-reset") {
+			try? FileManager.default.removeItem(at: localStoreURL)
+		}
+		
 		let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
 		let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
 		let localImageLoader = LocalFeedImageDataLoader(store: localStore)
--- EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -33,4 +33,13 @@ class EssentialAppUIAcceptanceTests: XCTestCase {
 		XCTAssertTrue(firstCachedImage.exists)
 	}
 	
+	func test_onLaunch_displaysEmptyFeedWhenCustomerHasNoConnectivityAndNoCache() {
+		let app = XCUIApplication()
+		app.launchArguments = ["-reset", "-connectivity", "offline"]
+		app.launch()
+
+		let feedCells = app.cells.matching(identifier: "feed-image-cell")
+		XCTAssertEqual(feedCells.count, 0)
+	}
+	
 }

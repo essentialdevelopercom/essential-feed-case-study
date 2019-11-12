4a078a25b9beccd3c97d9a20d3db971c5d35c436
Caio Zullo <caiozullo@gmail.com>
2019-11-12 15:08:41 +0000

    Reset cache in every test run to make sure tests run in a clean state and dont influence the result of other tests.

--- EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -8,7 +8,7 @@ class EssentialAppUIAcceptanceTests: XCTestCase {
 	
 	func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
 		let app = XCUIApplication()
-		
+		app.launchArguments = ["-reset"]
 		app.launch()
 		
 		let feedCells = app.cells.matching(identifier: "feed-image-cell")
@@ -20,6 +20,7 @@ class EssentialAppUIAcceptanceTests: XCTestCase {
 	
 	func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
 		let onlineApp = XCUIApplication()
+		onlineApp.launchArguments = ["-reset"]
 		onlineApp.launch()
 
 		let offlineApp = XCUIApplication()

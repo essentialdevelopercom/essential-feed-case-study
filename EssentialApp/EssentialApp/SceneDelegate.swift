6cc8d39e70a9cd9f4a4acd3d0e8a5c9c1f55c841
Caio Zullo <caiozullo@gmail.com>
2019-11-12 15:14:38 +0000

    Add #if DEBUG compilation directive to prevent test-specific code from being deployed in production

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -23,9 +23,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 			.defaultDirectoryURL()
 			.appendingPathComponent("feed-store.sqlite")
 
+		#if DEBUG
 		if CommandLine.arguments.contains("-reset") {
 			try? FileManager.default.removeItem(at: localStoreURL)
 		}
+		#endif
 		
 		let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
 		let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
@@ -45,16 +47,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	}
 	
 	private func makeRemoteClient() -> HTTPClient {
-		switch UserDefaults.standard.string(forKey: "connectivity") {
-		case "offline":
+		#if DEBUG
+		if UserDefaults.standard.string(forKey: "connectivity") == "offline" {
 			return AlwaysFailingHTTPClient()
-			
-		default:
-			return URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
-        }
+		}
+		#endif
+		
+		return URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
 	}
 }
 
+#if DEBUG
 private class AlwaysFailingHTTPClient: HTTPClient {
 	private class Task: HTTPClientTask {
 		func cancel() {}
@@ -65,3 +68,4 @@ private class AlwaysFailingHTTPClient: HTTPClient {
 		return Task()
 	}
 }
+#endif

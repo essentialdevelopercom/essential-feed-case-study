45f4ece0c2d18454de977f02fee848f583c67fab
Caio Zullo <caiozullo@gmail.com>
2019-11-12 15:00:07 +0000

    Displays cached feed on launch when customer has no connectivity

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -3,6 +3,7 @@
 //
 
 import UIKit
+import CoreData
 import EssentialFeed
 import EssentialFeediOS
 
@@ -14,12 +15,49 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 		
 		let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
 		
-		let remoteClient = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+		let remoteClient = makeRemoteClient()
 		let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: remoteClient)
 		let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
-				
+		
+		let localStoreURL = NSPersistentContainer
+			.defaultDirectoryURL()
+			.appendingPathComponent("feed-store.sqlite")
+
+		let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
+		let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
+		let localImageLoader = LocalFeedImageDataLoader(store: localStore)
+		
 		window?.rootViewController = FeedUIComposer.feedComposedWith(
-			feedLoader: remoteFeedLoader,
-			imageLoader: remoteImageLoader)
+			feedLoader: FeedLoaderWithFallbackComposite(
+				primary: FeedLoaderCacheDecorator(
+					decoratee: remoteFeedLoader,
+					cache: localFeedLoader),
+				fallback: localFeedLoader),
+			imageLoader: FeedImageDataLoaderWithFallbackComposite(
+				primary: localImageLoader,
+				fallback: FeedImageDataLoaderCacheDecorator(
+					decoratee: remoteImageLoader,
+					cache: localImageLoader)))
+	}
+	
+	private func makeRemoteClient() -> HTTPClient {
+		switch UserDefaults.standard.string(forKey: "connectivity") {
+		case "offline":
+			return AlwaysFailingHTTPClient()
+			
+		default:
+			return URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        }
+	}
+}
+
+private class AlwaysFailingHTTPClient: HTTPClient {
+	private class Task: HTTPClientTask {
+		func cancel() {}
+	}
+	
+	func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
+		completion(.failure(NSError(domain: "offline", code: 0)))
+		return Task()
 	}
 }
--- EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -18,4 +18,19 @@ class EssentialAppUIAcceptanceTests: XCTestCase {
 		XCTAssertTrue(firstImage.exists)
 	}
 	
+	func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
+		let onlineApp = XCUIApplication()
+		onlineApp.launch()
+
+		let offlineApp = XCUIApplication()
+		offlineApp.launchArguments = ["-connectivity", "offline"]
+		offlineApp.launch()
+
+		let cachedFeedCells = offlineApp.cells.matching(identifier: "feed-image-cell")
+		XCTAssertEqual(cachedFeedCells.count, 22)
+		
+		let firstCachedImage = offlineApp.images.matching(identifier: "feed-image-view").firstMatch
+		XCTAssertTrue(firstCachedImage.exists)
+	}
+	
 }

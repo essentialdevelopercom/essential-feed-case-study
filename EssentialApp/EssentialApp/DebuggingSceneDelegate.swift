87e3d1bca93ee8d3f1f2822e28cd4df60ef13466
Caio Zullo <caiozullo@gmail.com>
2019-11-12 15:37:17 +0000

    Intercept HTTP requests with canned responses during UI tests to eliminate network flakiness - We can now run UI tests without internet connection.

--- EssentialApp/EssentialApp/DebuggingSceneDelegate.swift
+++ EssentialApp/EssentialApp/DebuggingSceneDelegate.swift
@@ -18,21 +18,66 @@ class DebuggingSceneDelegate: SceneDelegate {
 	}
 	
 	override func makeRemoteClient() -> HTTPClient {
-		if UserDefaults.standard.string(forKey: "connectivity") == "offline" {
-			return AlwaysFailingHTTPClient()
+		if let connectivity = UserDefaults.standard.string(forKey: "connectivity") {
+			return DebuggingHTTPClient(connectivity: connectivity)
 		}
 		return super.makeRemoteClient()
 	}
 }
 
-private class AlwaysFailingHTTPClient: HTTPClient {
+private class DebuggingHTTPClient: HTTPClient {
 	private class Task: HTTPClientTask {
 		func cancel() {}
 	}
 	
+	private let connectivity: String
+	
+	init(connectivity: String) {
+		self.connectivity = connectivity
+	}
+
 	func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-		completion(.failure(NSError(domain: "offline", code: 0)))
+		switch connectivity {
+		case "online":
+			completion(.success(makeSuccessfulResponse(for: url)))
+			
+		default:
+			completion(.failure(NSError(domain: "offline", code: 0)))
+		}
 		return Task()
+	}
+	
+	private func makeSuccessfulResponse(for url: URL) -> (Data, HTTPURLResponse) {
+		let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)!
+		return (makeData(for: url), response)
+	}
+	
+	private func makeData(for url: URL) -> Data {
+		switch url.absoluteString {
+		case "http://image.com":
+			return makeImageData()
+			
+		default:
+			return makeFeedData()
+		}
+	}
+	
+	private func makeImageData() -> Data {
+		let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+		UIGraphicsBeginImageContext(rect.size)
+		let context = UIGraphicsGetCurrentContext()!
+		context.setFillColor(UIColor.red.cgColor)
+		context.fill(rect)
+		let img = UIGraphicsGetImageFromCurrentImageContext()
+		UIGraphicsEndImageContext()
+		return img!.pngData()!
+	}
+	
+	private func makeFeedData() -> Data {
+		return try! JSONSerialization.data(withJSONObject: ["items": [
+			["id": UUID().uuidString, "image": "http://image.com"],
+			["id": UUID().uuidString, "image": "http://image.com"]
+		]])
 	}
 }
 #endif
--- EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -8,11 +8,11 @@ class EssentialAppUIAcceptanceTests: XCTestCase {
 	
 	func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
 		let app = XCUIApplication()
-		app.launchArguments = ["-reset"]
+		app.launchArguments = ["-reset", "-connectivity", "online"]
 		app.launch()
 		
 		let feedCells = app.cells.matching(identifier: "feed-image-cell")
-		XCTAssertEqual(feedCells.count, 22)
+		XCTAssertEqual(feedCells.count, 2)
 		
 		let firstImage = app.images.matching(identifier: "feed-image-view").firstMatch
 		XCTAssertTrue(firstImage.exists)
@@ -20,7 +20,7 @@ class EssentialAppUIAcceptanceTests: XCTestCase {
 	
 	func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
 		let onlineApp = XCUIApplication()
-		onlineApp.launchArguments = ["-reset"]
+		onlineApp.launchArguments = ["-reset", "-connectivity", "online"]
 		onlineApp.launch()
 
 		let offlineApp = XCUIApplication()
@@ -28,7 +28,7 @@ class EssentialAppUIAcceptanceTests: XCTestCase {
 		offlineApp.launch()
 
 		let cachedFeedCells = offlineApp.cells.matching(identifier: "feed-image-cell")
-		XCTAssertEqual(cachedFeedCells.count, 22)
+		XCTAssertEqual(cachedFeedCells.count, 2)
 		
 		let firstCachedImage = offlineApp.images.matching(identifier: "feed-image-view").firstMatch
 		XCTAssertTrue(firstCachedImage.exists)

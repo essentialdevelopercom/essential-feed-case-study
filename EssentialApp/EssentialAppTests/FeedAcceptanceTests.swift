edd0e49009b23ae6d5b335f4bcba06371b5cf85a
Caio Zullo <caiozullo@gmail.com>
2019-12-06 15:55:38 +0000

    Move `HTTPClientStub` to a shared scope in a separate file

--- EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
+++ EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
@@ -70,31 +70,6 @@ class FeedAcceptanceTests: XCTestCase {
 		let sut = SceneDelegate(httpClient: HTTPClientStub.offline, store: store)
 		sut.sceneWillResignActive(UIApplication.shared.connectedScenes.first!)
 	}
-
-	private class HTTPClientStub: HTTPClient {
-		private class Task: HTTPClientTask {
-			func cancel() {}
-		}
-		
-		private let stub: (URL) -> HTTPClient.Result
-				
-		init(stub: @escaping (URL) -> HTTPClient.Result) {
-			self.stub = stub
-		}
-
-		func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-			completion(stub(url))
-			return Task()
-		}
-		
-		static var offline: HTTPClientStub {
-			HTTPClientStub(stub: { _ in .failure(NSError(domain: "offline", code: 0)) })
-		}
-		
-		static func online(_ stub: @escaping (URL) -> (Data, HTTPURLResponse)) -> HTTPClientStub {
-			HTTPClientStub { url in .success(stub(url)) }
-		}
-	}
 	
 	private class InMemoryFeedStore: FeedStore, FeedImageDataStore {
 		private(set) var feedCache: CachedFeed?
---
+++ EssentialApp/EssentialAppTests/Helpers/HTTPClientStub.swift
@@ -0,0 +1,33 @@
+//
+//  Copyright © 2019 Essential Developer. All rights reserved.
+//
+
+import Foundation
+import EssentialFeed
+
+class HTTPClientStub: HTTPClient {
+	private class Task: HTTPClientTask {
+		func cancel() {}
+	}
+	
+	private let stub: (URL) -> HTTPClient.Result
+			
+	init(stub: @escaping (URL) -> HTTPClient.Result) {
+		self.stub = stub
+	}
+
+	func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
+		completion(stub(url))
+		return Task()
+	}
+}
+
+extension HTTPClientStub {
+	static var offline: HTTPClientStub {
+		HTTPClientStub(stub: { _ in .failure(NSError(domain: "offline", code: 0)) })
+	}
+	
+	static func online(_ stub: @escaping (URL) -> (Data, HTTPURLResponse)) -> HTTPClientStub {
+		HTTPClientStub { url in .success(stub(url)) }
+	}
+}

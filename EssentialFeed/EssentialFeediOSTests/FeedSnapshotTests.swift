5742d6d2e27aabd5aca4f73d18ff7ca8576a35f1
Caio Zullo <caiozullo@gmail.com>
2019-12-16 16:27:13 +0000

    Record feed with error message snapshot

--- EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
@@ -24,6 +24,14 @@ class FeedSnapshotTests: XCTestCase {
 		record(snapshot: sut.snapshot(), named: "FEED_WITH_CONTENT")
 	}
 	
+	func test_feedWithErrorMessage() {
+		let sut = makeSUT()
+
+		sut.display(.error(message: "This is a\nmulti-line\nerror message"))
+
+		record(snapshot: sut.snapshot(), named: "FEED_WITH_ERROR_MESSAGE")
+	}
+	
 	// MARK: - Helpers
 
 	private func makeSUT() -> FeedViewController {

39262b62963ce9b4c1e71dc762b12a676483243a
Caio Zullo <caiozullo@gmail.com>
2020-06-02 14:58:57 +0100

    Extract layout cycle steps into a shared helper extension

--- EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests+Assertions.swift
+++ EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests+Assertions.swift
@@ -9,8 +9,7 @@ import EssentialFeediOS
 extension FeedUIIntegrationTests {
 
 	func assertThat(_ sut: FeedViewController, isRendering feed: [FeedImage], file: StaticString = #file, line: UInt = #line) {
-        sut.tableView.layoutIfNeeded()
-        RunLoop.main.run(until: Date())
+        sut.view.enforceLayoutCycle()
         
 		guard sut.numberOfRenderedFeedImageViews() == feed.count else {
 			return XCTFail("Expected \(feed.count) images, got \(sut.numberOfRenderedFeedImageViews()) instead.", file: file, line: line)
---
+++ EssentialApp/EssentialAppTests/Helpers/UIView+TestHelpers.swift
@@ -0,0 +1,12 @@
+//
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func enforceLayoutCycle() {
+        layoutIfNeeded()
+        RunLoop.current.run(until: Date())
+    }
+}

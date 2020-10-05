7ab2c49ec55e4f9066546bae6b6b7e8996af34b1
Caio Zullo <caiozullo@gmail.com>
2020-10-05 20:02:42 +0100

    Update Feed API URL

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -52,7 +52,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	}
     
     private func makeRemoteFeedLoaderWithLocalFallback() -> AnyPublisher<[FeedImage], Error> {
-        let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
+        let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
                         
         return httpClient
             .getPublisher(url: remoteURL)
--- EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
+++ EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
@@ -77,12 +77,15 @@ class FeedAcceptanceTests: XCTestCase {
 	}
 	
 	private func makeData(for url: URL) -> Data {
-		switch url.absoluteString {
-		case "http://image.com":
+        switch url.path {
+        case "/image-1", "/image-2":
 			return makeImageData()
-			
+        
+        case "/essential-feed/v1/feed":
+            return makeFeedData()
+
 		default:
-			return makeFeedData()
+            return Data()
 		}
 	}
 	
@@ -92,8 +95,8 @@ class FeedAcceptanceTests: XCTestCase {
 	
 	private func makeFeedData() -> Data {
 		return try! JSONSerialization.data(withJSONObject: ["items": [
-			["id": UUID().uuidString, "image": "http://image.com"],
-			["id": UUID().uuidString, "image": "http://image.com"]
+            ["id": "2AB2AE66-A4B7-4A16-B374-51BBAC8DB086", "image": "http://feed.com/image-1"],
+            ["id": "A28F5FE3-27A7-44E9-8DF5-53742D0E4A5A", "image": "http://feed.com/image-2"]
 		]])
 	}
 

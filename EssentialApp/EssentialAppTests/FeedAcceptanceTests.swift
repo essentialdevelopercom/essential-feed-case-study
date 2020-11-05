7f212baa7b1460ef01f218e8e118a1919703efbf
Caio Zullo <caiozullo@gmail.com>
2020-11-05 18:59:38 +0000

    Set small window frame to prevent loading content ahead of time during tests

--- EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
+++ EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
@@ -87,7 +87,7 @@ class FeedAcceptanceTests: XCTestCase {
 		store: InMemoryFeedStore = .empty
 	) -> ListViewController {
 		let sut = SceneDelegate(httpClient: httpClient, store: store)
-		sut.window = UIWindow()
+		sut.window = UIWindow(frame: CGRect(x: 0, y: 0, width: 1, height: 1))
 		sut.configureWindow()
 		
 		let nav = sut.window?.rootViewController as? UINavigationController

1241737409a7e055aac48ed0d0b46a9a33a8bf50
Caio Zullo <caiozullo@gmail.com>
2020-11-06 16:35:38 +0000

    Reuse existing cell controllers to avoid unnecessary reallocation of resources

--- EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
@@ -479,6 +479,11 @@ class FeedUIIntegrationTests: XCTestCase {
 		sut.simulateFeedImageViewNotVisible(at: 0)
 		sut.simulateFeedImageViewVisible(at: 0)
 		XCTAssertEqual(loader.loadedImageURLs, [image.url, image.url, image.url], "Expected third request when visible after canceling previous complete")
+		
+		sut.simulateLoadMoreFeedAction()
+		loader.completeLoadMore(with: [image, makeImage()])
+		sut.simulateFeedImageViewVisible(at: 0)
+		XCTAssertEqual(loader.loadedImageURLs, [image.url, image.url, image.url], "Expected no request until previous completes")
 	}
 
 	// MARK: - Helpers

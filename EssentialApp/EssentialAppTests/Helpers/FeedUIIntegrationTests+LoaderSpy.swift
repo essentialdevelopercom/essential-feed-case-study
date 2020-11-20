ee4325158ba19af8a5abda19ec066a53f79472bb
Caio Zullo <caiozullo@gmail.com>
2020-11-20 11:03:04 +0000

    Port all `LoaderSpy` methods to Combine publishers

--- EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests+LoaderSpy.swift
+++ EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests+LoaderSpy.swift
@@ -9,7 +9,7 @@ import Combine
 
 extension FeedUIIntegrationTests {
 	
-	class LoaderSpy: FeedImageDataLoader {
+	class LoaderSpy {
 		
 		// MARK: - FeedLoader
 		
@@ -63,33 +63,30 @@ extension FeedUIIntegrationTests {
         }
 		
 		// MARK: - FeedImageDataLoader
-		
-		private struct TaskSpy: FeedImageDataLoaderTask {
-			let cancelCallback: () -> Void
-			func cancel() {
-				cancelCallback()
-			}
-		}
-		
-		private var imageRequests = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
-		
+				
+		private var imageRequests = [(url: URL, publisher: PassthroughSubject<Data, Error>)]()
+
 		var loadedImageURLs: [URL] {
 			return imageRequests.map { $0.url }
 		}
 		
 		private(set) var cancelledImageURLs = [URL]()
 		
-		func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-			imageRequests.append((url, completion))
-			return TaskSpy { [weak self] in self?.cancelledImageURLs.append(url) }
+		func loadImageDataPublisher(from url: URL) -> AnyPublisher<Data, Error> {
+			let publisher = PassthroughSubject<Data, Error>()
+			imageRequests.append((url, publisher))
+			return publisher.handleEvents(receiveCancel: { [weak self] in
+				self?.cancelledImageURLs.append(url)
+			}).eraseToAnyPublisher()
 		}
 		
 		func completeImageLoading(with imageData: Data = Data(), at index: Int = 0) {
-			imageRequests[index].completion(.success(imageData))
+			imageRequests[index].publisher.send(imageData)
+			imageRequests[index].publisher.send(completion: .finished)
 		}
 		
 		func completeImageLoadingWithError(at index: Int = 0) {
-			imageRequests[index].completion(.failure(anyNSError()))
+			imageRequests[index].publisher.send(completion: .failure(anyNSError()))
 		}
 	}
 	

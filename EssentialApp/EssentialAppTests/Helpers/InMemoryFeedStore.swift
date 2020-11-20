2e54e3067e9a6a1f0cc751754bb25f06b9f2b6ad
Caio Zullo <caiozullo@gmail.com>
2020-11-20 15:51:23 +0000

    Make in memory FeedImageDataStore implementation sync

--- EssentialApp/EssentialAppTests/Helpers/InMemoryFeedStore.swift
+++ EssentialApp/EssentialAppTests/Helpers/InMemoryFeedStore.swift
@@ -31,13 +31,12 @@ extension InMemoryFeedStore: FeedStore {
 }
 
 extension InMemoryFeedStore: FeedImageDataStore {
-	func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+	func insert(_ data: Data, for url: URL) throws {
 		feedImageDataCache[url] = data
-		completion(.success(()))
 	}
 	
-	func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
-		completion(.success(feedImageDataCache[url]))
+	func retrieve(dataForURL url: URL) throws -> Data? {
+		feedImageDataCache[url]
 	}
 }
 

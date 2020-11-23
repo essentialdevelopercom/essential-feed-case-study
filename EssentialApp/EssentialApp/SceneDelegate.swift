8f3eed12fb0f6ea1a688e534f9e1fa091c260ea4
Caio Zullo <caiozullo@gmail.com>
2020-11-23 12:45:27 +0000

    Subscribe upstream store subscriptions in a background queue to avoid blocking the main queue (tests still run synchronously in the main queue with the immediate scheduler)

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -95,6 +95,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 			.caching(to: localFeedLoader)
 			.fallback(to: localFeedLoader.loadPublisher)
 			.map(makeFirstPage)
+			.subscribe(on: scheduler)
 			.eraseToAnyPublisher()
 	}
 	
@@ -103,8 +104,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 			.zip(makeRemoteFeedLoader(after: last))
 			.map { (cachedItems, newItems) in
 				(cachedItems + newItems, newItems.last)
-			}.map(makePage)
+			}
+			.map(makePage)
 			.caching(to: localFeedLoader)
+			.subscribe(on: scheduler)
+			.eraseToAnyPublisher()
 	}
 	
 	private func makeRemoteFeedLoader(after: FeedImage? = nil) -> AnyPublisher<[FeedImage], Error> {

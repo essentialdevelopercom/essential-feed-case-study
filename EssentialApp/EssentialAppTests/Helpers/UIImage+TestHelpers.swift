c9151fd0bdec201ec6c5fbe74e6ced3543e26a24
Caio Zullo <caiozullo@gmail.com>
2022-11-18 10:14:26 -0300

    iOS 16.1+ fix: Set explicit test images scale to 1 to ensure they all have the same scale (so their pngData can be compared pixel by pixel)

--- EssentialApp/EssentialAppTests/Helpers/UIImage+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/UIImage+TestHelpers.swift
@@ -7,12 +7,12 @@ import UIKit
 extension UIImage {
 	static func make(withColor color: UIColor) -> UIImage {
 		let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
-		UIGraphicsBeginImageContext(rect.size)
-		let context = UIGraphicsGetCurrentContext()!
-		context.setFillColor(color.cgColor)
-		context.fill(rect)
-		let img = UIGraphicsGetImageFromCurrentImageContext()
-		UIGraphicsEndImageContext()
-		return img!
+		let format = UIGraphicsImageRendererFormat()
+		format.scale = 1
+
+		return UIGraphicsImageRenderer(size: rect.size, format: format).image { rendererContext in
+			color.setFill()
+			rendererContext.fill(rect)
+		}
 	}
 }

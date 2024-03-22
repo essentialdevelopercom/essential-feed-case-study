a1192714def341d637c45469e60ae14112c1b3c2
Caio Zullo <caiozullo@gmail.com>
2024-03-22 11:16:41 +0200

    Replace `try!` with throwing test methods
    `try!` can cause crashes, which stops the test execution. When possible, it's best to throw errors in test methods, so you get a failing test rather than a crash.

--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
@@ -7,14 +7,14 @@ import EssentialFeed
 
 class CoreDataFeedImageDataStoreTests: XCTestCase {
 	
-	func test_retrieveImageData_deliversNotFoundWhenEmpty() {
-		let sut = makeSUT()
+	func test_retrieveImageData_deliversNotFoundWhenEmpty() throws {
+		let sut = try makeSUT()
 		
 		expect(sut, toCompleteRetrievalWith: notFound(), for: anyURL())
 	}
 	
-	func test_retrieveImageData_deliversNotFoundWhenStoredDataURLDoesNotMatch() {
-		let sut = makeSUT()
+	func test_retrieveImageData_deliversNotFoundWhenStoredDataURLDoesNotMatch() throws {
+		let sut = try makeSUT()
 		let url = URL(string: "http://a-url.com")!
 		let nonMatchingURL = URL(string: "http://another-url.com")!
 		
@@ -23,8 +23,8 @@ class CoreDataFeedImageDataStoreTests: XCTestCase {
 		expect(sut, toCompleteRetrievalWith: notFound(), for: nonMatchingURL)
 	}
 	
-	func test_retrieveImageData_deliversFoundDataWhenThereIsAStoredImageDataMatchingURL() {
-		let sut = makeSUT()
+	func test_retrieveImageData_deliversFoundDataWhenThereIsAStoredImageDataMatchingURL() throws {
+		let sut = try makeSUT()
 		let storedData = anyData()
 		let matchingURL = URL(string: "http://a-url.com")!
 		
@@ -33,8 +33,8 @@ class CoreDataFeedImageDataStoreTests: XCTestCase {
 		expect(sut, toCompleteRetrievalWith: found(storedData), for: matchingURL)
 	}
 	
-	func test_retrieveImageData_deliversLastInsertedValue() {
-		let sut = makeSUT()
+	func test_retrieveImageData_deliversLastInsertedValue() throws {
+		let sut = try makeSUT()
 		let firstStoredData = Data("first".utf8)
 		let lastStoredData = Data("last".utf8)
 		let url = URL(string: "http://a-url.com")!
@@ -47,9 +47,9 @@ class CoreDataFeedImageDataStoreTests: XCTestCase {
 	
 	// - MARK: Helpers
 	
-	private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CoreDataFeedStore {
+	private func makeSUT(file: StaticString = #filePath, line: UInt = #line) throws -> CoreDataFeedStore {
 		let storeURL = URL(fileURLWithPath: "/dev/null")
-		let sut = try! CoreDataFeedStore(storeURL: storeURL)
+		let sut = try CoreDataFeedStore(storeURL: storeURL)
 		trackForMemoryLeaks(sut, file: file, line: line)
 		return sut
 	}
--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -7,77 +7,77 @@ import EssentialFeed
 
 class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
 	
-	func test_retrieve_deliversEmptyOnEmptyCache() {
-		let sut = makeSUT()
+	func test_retrieve_deliversEmptyOnEmptyCache() throws {
+		let sut = try makeSUT()
 		
 		assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
 	}
 	
-	func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-		let sut = makeSUT()
+	func test_retrieve_hasNoSideEffectsOnEmptyCache() throws {
+		let sut = try makeSUT()
 		
 		assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
 	}
 	
-	func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
-		let sut = makeSUT()
+	func test_retrieve_deliversFoundValuesOnNonEmptyCache() throws {
+		let sut = try makeSUT()
 		
 		assertThatRetrieveDeliversFoundValuesOnNonEmptyCache(on: sut)
 	}
 	
-	func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
-		let sut = makeSUT()
+	func test_retrieve_hasNoSideEffectsOnNonEmptyCache() throws {
+		let sut = try makeSUT()
 		
 		assertThatRetrieveHasNoSideEffectsOnNonEmptyCache(on: sut)
 	}
 	
-	func test_insert_deliversNoErrorOnEmptyCache() {
-		let sut = makeSUT()
+	func test_insert_deliversNoErrorOnEmptyCache() throws {
+		let sut = try makeSUT()
 		
 		assertThatInsertDeliversNoErrorOnEmptyCache(on: sut)
 	}
 	
-	func test_insert_deliversNoErrorOnNonEmptyCache() {
-		let sut = makeSUT()
+	func test_insert_deliversNoErrorOnNonEmptyCache() throws {
+		let sut = try makeSUT()
 		
 		assertThatInsertDeliversNoErrorOnNonEmptyCache(on: sut)
 	}
 	
-	func test_insert_overridesPreviouslyInsertedCacheValues() {
-		let sut = makeSUT()
+	func test_insert_overridesPreviouslyInsertedCacheValues() throws {
+		let sut = try makeSUT()
 		
 		assertThatInsertOverridesPreviouslyInsertedCacheValues(on: sut)
 	}
 	
-	func test_delete_deliversNoErrorOnEmptyCache() {
-		let sut = makeSUT()
+	func test_delete_deliversNoErrorOnEmptyCache() throws {
+		let sut = try makeSUT()
 		
 		assertThatDeleteDeliversNoErrorOnEmptyCache(on: sut)
 	}
 	
-	func test_delete_hasNoSideEffectsOnEmptyCache() {
-		let sut = makeSUT()
+	func test_delete_hasNoSideEffectsOnEmptyCache() throws {
+		let sut = try makeSUT()
 		
 		assertThatDeleteHasNoSideEffectsOnEmptyCache(on: sut)
 	}
 	
-	func test_delete_deliversNoErrorOnNonEmptyCache() {
-		let sut = makeSUT()
+	func test_delete_deliversNoErrorOnNonEmptyCache() throws {
+		let sut = try makeSUT()
 		
 		assertThatDeleteDeliversNoErrorOnNonEmptyCache(on: sut)
 	}
 	
-	func test_delete_emptiesPreviouslyInsertedCache() {
-		let sut = makeSUT()
+	func test_delete_emptiesPreviouslyInsertedCache() throws {
+		let sut = try makeSUT()
 		
 		assertThatDeleteEmptiesPreviouslyInsertedCache(on: sut)
 	}
 	
 	// - MARK: Helpers
 	
-	private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> FeedStore {
+	private func makeSUT(file: StaticString = #filePath, line: UInt = #line) throws -> FeedStore {
 		let storeURL = URL(fileURLWithPath: "/dev/null")
-		let sut = try! CoreDataFeedStore(storeURL: storeURL)
+		let sut = try CoreDataFeedStore(storeURL: storeURL)
 		trackForMemoryLeaks(sut, file: file, line: line)
 		return sut
 	}

34c7ed898886f38a53371b0eabf74d02b68f7779
Swift En Profundidad <freelancemerlos@gmail.com>
2025-04-22 12:30:00 +0200

    Move UserLoginUseCase to Authentication Feature folder

---
+++ EssentialFeed/EssentialFeed/Authentication Feature/UserLoginUseCase.swift
@@ -0,0 +1,56 @@
+import Foundation
+
+public struct LoginCredentials {
+    public let email: String
+    public let password: String
+    public init(email: String, password: String) {
+        self.email = email
+        self.password = password
+    }
+}
+
+public struct LoginResponse: Equatable {
+    public let token: String
+    public init(token: String) {
+        self.token = token
+    }
+}
+
+public protocol AuthAPI {
+    func login(with credentials: LoginCredentials) async -> Result<LoginResponse, LoginError>
+}
+
+public enum LoginError: Error, Equatable {
+    case invalidCredentials
+    case network
+}
+
+public protocol LoginSuccessObserver {
+    func didLoginSuccessfully(response: LoginResponse)
+}
+
+public protocol LoginFailureObserver {
+    func didFailLogin(error: LoginError)
+}
+
+public final class UserLoginUseCase {
+    private let api: AuthAPI
+    private let successObserver: LoginSuccessObserver?
+    private let failureObserver: LoginFailureObserver?
+    public init(api: AuthAPI, successObserver: LoginSuccessObserver? = nil, failureObserver: LoginFailureObserver? = nil) {
+        self.api = api
+        self.successObserver = successObserver
+        self.failureObserver = failureObserver
+    }
+    public func login(with credentials: LoginCredentials) async -> Result<LoginResponse, LoginError> {
+        let result = await api.login(with: credentials)
+        switch result {
+        case let .success(response):
+            self.successObserver?.didLoginSuccessfully(response: response)
+            return .success(response)
+        case let .failure(error):
+            self.failureObserver?.didFailLogin(error: error)
+            return .failure(error)
+        }
+    }
+}

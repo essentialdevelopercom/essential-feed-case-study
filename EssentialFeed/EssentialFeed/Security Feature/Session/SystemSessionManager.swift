7822cc84eb3fc93ca2ba338cf5be84fa6386ca9a
Swift En Profundidad <freelancemerlos@gmail.com>
2025-04-23 11:21:00 +0200

    Add SystemSessionManager with store delegation and tests

---
+++ EssentialFeed/EssentialFeed/Security Feature/Session/SystemSessionManager.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+public protocol SessionStore {
+    func saveSession(userID: String, token: String, date: Date)
+}
+
+public final class SystemSessionManager: SessionManaging {
+    private let store: SessionStore
+
+    public init(store: SessionStore) {
+        self.store = store
+    }
+
+    public func registerSession(userID: String, token: String, date: Date) {
+        store.saveSession(userID: userID, token: token, date: date)
+    }
+}

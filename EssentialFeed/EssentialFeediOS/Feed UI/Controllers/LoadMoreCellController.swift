7575900b826de4f3033e6b65081b89309e077c11
Caio Zullo <caiozullo@gmail.com>
2025-10-24 13:32:31 +0200

    Set SWIFT_STRICT_CONCURRENCY to `complete` in `EssentialFeediOS` and fix all warnings

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/LoadMoreCellController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/LoadMoreCellController.swift
@@ -27,9 +27,11 @@ public class LoadMoreCellController: NSObject, UITableViewDataSource, UITableVie
 		reloadIfNeeded()
 		
 		offsetObserver = tableView.observe(\.contentOffset, options: .new) { [weak self] (tableView, _) in
-			guard tableView.isDragging else { return }
-			
-			self?.reloadIfNeeded()
+			MainActor.assumeIsolated {
+				guard tableView.isDragging else { return }
+				
+				self?.reloadIfNeeded()
+			}
 		}
 	}
 	

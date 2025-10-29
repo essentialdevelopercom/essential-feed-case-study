1011dea091e0d1e7f0d40c318c397799e7ec7503
Caio Zullo <caiozullo@gmail.com>
2025-10-29 12:07:01 +0100

    Introduce new HTTPClient async method and deprecate the closure-based one

--- EssentialFeed/EssentialFeed/Shared API/HTTPClient.swift
+++ EssentialFeed/EssentialFeed/Shared API/HTTPClient.swift
@@ -14,5 +14,23 @@ public protocol HTTPClient {
 	/// The completion handler can be invoked in any thread.
 	/// Clients are responsible to dispatch to appropriate threads, if needed.
 	@discardableResult
+	@available(*, deprecated, message: "Use async alternative")
 	func get(from url: URL, completion: @Sendable @escaping (Result) -> Void) -> HTTPClientTask
+	
+	func get(from url: URL) async throws -> (Data, HTTPURLResponse)
+}
+
+public extension HTTPClient {
+	func get(from url: URL) async throws -> (Data, HTTPURLResponse) {
+		nonisolated(unsafe) var task: HTTPClientTask?
+		return try await withTaskCancellationHandler {
+			return try await withCheckedThrowingContinuation { continuation in
+				task = get(from: url) { result in
+					continuation.resume(with: result)
+				}
+			}
+		} onCancel: {
+			task?.cancel()
+		}
+	}
 }

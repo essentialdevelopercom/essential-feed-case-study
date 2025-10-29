fdb1a2cb9f04810a8d290ea2cb641372ad6aa251
Caio Zullo <caiozullo@gmail.com>
2025-10-29 12:43:47 +0100

    Remove deprecated `HTTPClient` closured-based API in favor of the new async API

--- EssentialApp/EssentialAppTests/Helpers/HTTPClientStub.swift
+++ EssentialApp/EssentialAppTests/Helpers/HTTPClientStub.swift
@@ -6,23 +6,14 @@ import Foundation
 import EssentialFeed
 
 class HTTPClientStub: HTTPClient {
-	private class Task: HTTPClientTask {
-		func cancel() {}
-	}
+	private let stub: (URL) -> Result<(Data, HTTPURLResponse), Error>
 	
-	private let stub: (URL) -> HTTPClient.Result
-	
-	init(stub: @escaping (URL) -> HTTPClient.Result) {
+	init(stub: @escaping (URL) -> Result<(Data, HTTPURLResponse), Error>) {
 		self.stub = stub
 	}
 	
 	func get(from url: URL) async throws -> (Data, HTTPURLResponse) {
 		try stub(url).get()
-	}
-	
-	func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-		completion(stub(url))
-		return Task()
 	}
 }
 
--- EssentialFeed/EssentialFeed/Shared API/HTTPClient.swift
+++ EssentialFeed/EssentialFeed/Shared API/HTTPClient.swift
@@ -4,34 +4,6 @@
 
 import Foundation
 
-public protocol HTTPClientTask {
-	func cancel()
-}
-
 public protocol HTTPClient {
-	typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
-	
-	/// The completion handler can be invoked in any thread.
-	/// Clients are responsible to dispatch to appropriate threads, if needed.
-	@discardableResult
-	@available(*, deprecated, message: "Use async alternative")
-	func get(from url: URL, completion: @Sendable @escaping (Result) -> Void) -> HTTPClientTask
-	
 	func get(from url: URL) async throws -> (Data, HTTPURLResponse)
-}
-
-@MainActor
-public extension HTTPClient {
-	func get(from url: URL) async throws -> (Data, HTTPURLResponse) {
-		nonisolated(unsafe) var task: HTTPClientTask?
-		return try await withTaskCancellationHandler {
-			return try await withCheckedThrowingContinuation { continuation in
-				task = get(from: url) { result in
-					continuation.resume(with: result)
-				}
-			}
-		} onCancel: {
-			task?.cancel()
-		}
-	}
 }
